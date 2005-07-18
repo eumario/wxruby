c6ef0fd67b5faf173882642d9db5f7e0598407c5
Kevin Smith <wxruby@qualitycode.com>
2005-07-18 02:07:11 +0000

    Handle a case where there were no matches (?)

--- wxruby2/swig/fixmodule.rb
+++ wxruby2/swig/fixmodule.rb
@@ -52,21 +52,25 @@ File.open(ARGV[0], "w") do | out |
         end
  
         if(line.index("rb_define_class_under"))
-            re = Regexp.new("\"(.*)\"")
-            core_name = re.match(line)[1]
-            this_wxklass = 'wx' + core_name
-            parent_wxklass = $parents[this_wxklass]
-            result = []
-            if(parent_wxklass)
-                parent_name = parent_wxklass[2..-1]
-                result << "    extern void Init_wx#{parent_name}();"
-                result << "    Init_wx#{parent_name}();"
-                result << "    //extern swig_class cWx#{parent_name};"
-                parent_klass = "cWx#{parent_name}.klass"
-                line = line.gsub(/rb_cObject/, parent_klass)
-            end
+            re = Regexp.new('\"(.*)\"')
+            match = re.match(line)
+            if match != nil
+              core_name = match[1]
+              this_wxklass = 'wx' + core_name
+              parent_wxklass = $parents[this_wxklass]
+              result = []
+              if(parent_wxklass)
+                  parent_name = parent_wxklass[2..-1]
+                  result << "    extern void Init_wx#{parent_name}();"
+                  result << "    Init_wx#{parent_name}();"
+                  result << "    //extern swig_class cWx#{parent_name};"
+                  parent_klass = "cWx#{parent_name}.klass"
+                  line = line.gsub(/rb_cObject/, parent_klass)
+              end
             result << line
             line = result.join("\n")
+            end
+  
         end
         
         out.puts(line)
