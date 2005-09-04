953caf63894deeab065045c748a54248af41e11e
Kevin Smith <wxruby@qualitycode.com>
2005-09-04 16:21:54 +0000

    Improve wxCheckListBox (thanks to Roy Sutton)

--- wxruby2/swig/classes/include/wxBogus.h
+++ wxruby2/swig/classes/include/wxBogus.h
@@ -1,7 +1,6 @@
+
 class wxBogus
 {
 public:
-	virtual void virt2() { printf("C++ virt2\n"); }
-	void nonvirt1() { printf("C++ nonvirt1\n"); virt2(); }
-	virtual void virt1() { printf("C++ virt1\n"); nonvirt1(); }
+	void TestDefaults(int  n, const void*  choices[], long style = 124);
 };
