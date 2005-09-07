e2e9540d3972cb7a149347e340d1d3208dee7bbf
Kevin Smith <wxruby@qualitycode.com>
2005-09-07 14:28:49 +0000

    Add back commented-out CheckListBox#get_item_height

--- wxruby2/swig/classes/include/wxCheckBox.h
+++ wxruby2/swig/classes/include/wxCheckBox.h
@@ -8,62 +8,17 @@
 class wxCheckBox : public wxControl
 {
 public:
-	/**
-	 * \brief Default constructor. 
-	*/
-
    wxCheckBox() ;
-	/**
-	 * \brief  
-	 * \param wxWindow*   
-	 * \param wxWindowID  
-	 * \param const wxString&   
-	 * \param const wxPoint&   
-	 * \param const wxSize&   
-	 * \param long  
-	 * \param const wxValidator&   
-	 * \param const wxString&   
-	*/
-
   wxCheckBox(wxWindow*  parent , wxWindowID  id , const wxString&  label , const wxPoint&  pos = wxDefaultPosition, const wxSize&  size = wxDefaultSize, long style = 0, const wxValidator&  val = wxDefaultValidator, const wxString&  name = wxT("checkBox"));
-	/**
-	 * \brief Destructor, destroying the checkbox. 
-	*/
-
   virtual  ~wxCheckBox() ;
-	/**
-	 * \brief Creates the checkbox for two-step construction. See  
-for details. 
-	 * \param wxWindow*   
-	 * \param wxWindowID  
-	 * \param const wxString&   
-	 * \param const wxPoint&   
-	 * \param const wxSize&   
-	 * \param long  
-	 * \param const wxValidator&   
-	 * \param const wxString&   
-	*/
-
   bool Create(wxWindow*  parent , wxWindowID  id , const wxString&  label , const wxPoint&  pos = wxDefaultPosition, const wxSize&  size = wxDefaultSize, long style = 0, const wxValidator&  val = wxDefaultValidator, const wxString&  name = wxT("checkBox"));
-	/**
-	 * \brief Gets the state of the checkbox. 
-	*/
-
   bool GetValue() const;
-	/**
-	 * \brief This is just a maybe more readable synonym for 
- : just as the latter, it returns 
-  if the checkbox is checked and   otherwise. 
-	*/
-
   bool IsChecked() const;
-	/**
-	 * \brief Sets the checkbox to the given state. This does not cause a
-wxEVT_COMMAND_CHECKBOX_CLICKED event to get emitted. 
-	 * \param const bool  
-	*/
-
   void SetValue(const bool  state ) ;
+
+  // GetItemHeight is defined for GTK and MSW but not Mac
+  // We should probably #if it, but for now we'll just take it out
+  //size_t GetItemHeight() const { return m_nItemHeight; }  
 };
 
 
