10f71a8a9a5d3c86898b5b806667ab6ba14960b9
Kevin Smith <wxruby@qualitycode.com>
2005-09-13 02:02:37 +0000

    Omit FontDialog.new(window) from Mac to avoid compile error

--- wxruby2/swig/classes/include/wxFontDialog.h
+++ wxruby2/swig/classes/include/wxFontDialog.h
@@ -1,79 +1,23 @@
-// wxFontDialog.h
-// This file was automatically generated
-// by extractxml.rb, part of the wxRuby project
-// Do not make changes directly to this file!
+//   Copyright 2004-2005 by Kevin Smith
+//   released under the MIT-style wxruby2 license
+
 
 #if !defined(_wxFontDialog_h_)
 #define _wxFontDialog_h_
 class wxFontDialog : public wxDialog
 {
 public:
-	/**
-	 * \brief  
-	*/
-
    wxFontDialog() ;
-	/**
-	 * \brief  
-	 * \param wxWindow*   
-	*/
-
+#if !defined(__WXMAC__)
    wxFontDialog(wxWindow*  parent ) ;
-	/**
-	 * \brief Constructor. Pass a parent window, and optionally the 
-  object to be used to initialize the dialog
-controls. If the default constructor is used, 
-  must be called before the dialog can be
-shown. 
-	 * \param wxWindow*   
-	 * \param const wxFontData&   
-	*/
-
+#endif
    wxFontDialog(wxWindow*  parent , const wxFontData&  data ) ;
-	/**
-	 * \brief  
-	*/
-
   bool Create() ;
-	/**
-	 * \brief  
-	 * \param wxWindow*   
-	*/
-
   bool Create(wxWindow*  parent ) ;
-	/**
-	 * \brief Creates the dialog if it the wxFontDialog object had been initialized using the
-default constructor. Returns   on success and   if an error
-occured. 
-	 * \param wxWindow*   
-	 * \param const wxFontData&   
-	*/
-
   bool Create(wxWindow*  parent , const wxFontData&  data ) ;
-	/**
-	 * \brief Destructor. 
-	*/
-
   virtual  ~wxFontDialog() ;
-	/**
-	 * \brief  
-	*/
-
   const wxFontData& GetFontData() const;
-	/**
-	 * \brief Returns the   associated with the font dialog. 
-	*/
-
   wxFontData& GetFontData() ;
-	/**
-	 * \brief Shows the dialog, returning   if the user pressed Ok, and 
-  otherwise.
-
-If the user cancels the dialog (ShowModal returns  ), no font
-will be created. If the user presses OK, a new wxFont will be created and
-stored in the font dialog's wxFontData structure. 
-	*/
-
   int ShowModal() ;
 };
 
