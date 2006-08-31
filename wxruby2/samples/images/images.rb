4db6a87023ef1eb29c0eb62d9e14f9b4e3b2955a
Alex Fenton <alex@pressure.to>
2006-08-31 17:32:33 +0000

    Fixed running from another directory; added explanatory notes; small ruby style method naming etc points

--- wxruby2/samples/images/images.rb
+++ wxruby2/samples/images/images.rb
@@ -3,19 +3,27 @@
 
 require 'wx'
 
+# This sample demonstrates how to draw an image from a file onto a
+# window. This one uses a small PNG file, but other formats such as JPEG
+# are supported - see documentation for more details.
+
 class MyFrame < Wx::Frame
   def initialize(title)
     super(nil, -1, title)
-    evt_paint do onPaint end
-    image = Wx::Image.new('paperclip.png')
+    evt_paint { on_paint }
+    
+    img_file = File.join( File.dirname(__FILE__), 'paperclip.png')
+    # first load the image into an Image object
+    image = Wx::Image.new(img_file)
+    # then create a Bitmap suitable for drawing
     @bitmap = Wx::Bitmap.new(image)
   end
 
-  def onPaint
+  def on_paint
     puts("onPaint begin")
     paint do | dc |
-	dc.clear
-	dc.draw_bitmap(@bitmap, 0, 0, false)
+      dc.clear
+      dc.draw_bitmap(@bitmap, 0, 0, false)
     end
     puts("onPaint end")
   end
