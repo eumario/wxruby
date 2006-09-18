195d8f97b30bed91a934b98fd546088ed04a5174
Alex Fenton <alex@pressure.to>
2006-09-18 22:55:44 +0000

    Versioning target + package naming system in rake & lib loader

--- wxruby2/lib/wx.rb
+++ wxruby2/lib/wx.rb
@@ -1,6 +1,12 @@
 # rakewx.rb
-#   Copyright 2004-2005 by Kevin Smith
+#   Copyright 2004-2006 by Kevin Smith
 #   released under the MIT-style wxruby2 license
+
+# This wrapper serves two functions:
+# 1. It allows apps to require 'wx' but still have the binary 
+#    library properly named wxruby2.so or wxruby2.dll
+# 2. Later, we can add wrapper classes and methods here that can 
+#    expose a friendlier API than the C++ code provides
 
 begin
 	require 'wxruby2'
@@ -10,16 +16,14 @@ rescue LoadError => e
 	exit(1)
 end
 
-# This wrapper serves two functions:
-# 1. It allows apps to require 'wx' but still have the binary 
-#    library properly named wxruby2.so or wxruby2.dll
-# 2. Later, we can add wrapper classes and methods here that can 
-#    expose a friendlier API than the C++ code provides
-
 Wx = Wxruby2
 
-Wx::WXRUBY_VERSION    = '0.0.35'
-Wx::WXWIDGETS_VERSION = '2.6.3'
+begin
+  require 'wx/version'
+rescue LoadError
+  Wx::WXWIDGETS_VERSION = '0.0.0'
+  Wx::WXRUBY_VERSION = '0.0.0'
+end
 
 class Wx::App
 	def on_assert(file, line, condition, message)
