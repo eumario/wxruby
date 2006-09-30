4b1b026df9ebf80d19d4f7303b7717e5217f7581
Alex Fenton <alex@pressure.to>
2006-09-30 00:13:37 +0000

    Use Scintilla for demo code viewer, if available (Roy Sutton)

--- samples/bigdemo/bigdemo.rb
+++ samples/bigdemo/bigdemo.rb
@@ -1,6 +1,15 @@
 # Demo created by Robert Carlin, 
 #  based on the wxPython demo by Robin Dunn
-#  Modified a bit by Kevin Smith
+#  Modified a bit by Kevin Smith and Roy Sutton
+
+#  TODO:
+#    Finish Scintilla integration
+#    Fix 'Find Next'
+#    Refactor editor pane
+#    More samples
+#    Finish 'rubification' of source
+#    Find out why expanding/collapsing a node deletes a character in scintilla
+#    Fix non-scintilla code viewer
 
 require 'wx'
 
@@ -199,8 +208,9 @@ $tree_list = [
 
 class DemoCodeViewer < Wx::TextCtrl
     def intialize(parent, id)
-        super(parent, id, "", Wx::DEFAULT_POSITION, Wx::DEFAULT_SIZE, 
-              Wx::TE_MULTILINE|Wx::TE_READONLY|Wx::HSCROLL|                                   Wx::TE_RICH|Wx::TE_NOHIDESEL)
+        super(parent, id, OVR_TEXT, Wx::DEFAULT_POSITION, Wx::Size.new(400, 400), 
+              Wx::TE_MULTILINE|Wx::TE_READONLY|Wx::HSCROLL|
+              Wx::TE_RICH|Wx::TE_NOHIDESEL)
     end
 end
 
@@ -304,13 +314,22 @@ class WxRubyDemo < Wx::Frame
                                 Wx::TE_RICH|Wx::TE_NOHIDESEL)
         @nb.add_page(@ovr, "Overview")
         
-        @txt = Wx::TextCtrl.new(@nb, -1, OVR_TEXT, Wx::DEFAULT_POSITION, 
-                                Wx::Size.new(400,400), 
-                                Wx::TE_MULTILINE|Wx::TE_READONLY|
-                                Wx::HSCROLL|Wx::TE_RICH|Wx::TE_NOHIDESEL)
-        @txt.set_max_length(0)
+        if Wx.const_defined?(:Scintilla)
+          panel, @txt = setup_editor(@nb)
+          @nb.add_page(panel, "Demo Code")
+        else
+          # FIXME: Should use DemoCodeViewer, but when we do (on Windows) it
+          #         puts all text on one line!  Can remove instance_eval
+          #         once that's resolved
+          @txt = Wx::TextCtrl.new(@nb, -1, OVR_TEXT, Wx::DEFAULT_POSITION, 
+                                  Wx::Size.new(400,400), 
+                                  Wx::TE_MULTILINE|Wx::TE_READONLY|
+                                  Wx::HSCROLL|Wx::TE_RICH|Wx::TE_NOHIDESEL)
+          @txt.instance_eval { alias clear_all clear }
+          @txt.set_max_length(0)
+          @nb.add_page(@txt, "Demo Code")
+        end
         
-        @nb.add_page(@txt, "Demo Code")
         get_demo_file(__FILE__)
         
         @log = Wx::TextCtrl.new(splitter2, -1, "", Wx::DEFAULT_POSITION, 
@@ -425,7 +444,7 @@ class WxRubyDemo < Wx::Frame
                 end
             else
                 @ovr.set_value("")
-                @txt.clear()
+                @txt.clear_all
                 @window = nil
             end
         end
@@ -433,10 +452,13 @@ class WxRubyDemo < Wx::Frame
     end
     
     def get_demo_file(fileName)
+        @filename = fileName
         @txt.clear()
         @txt.load_file(fileName)
-        @txt.set_insertion_point(0)
-        @txt.show_position(0)
+        if @txt.is_a?(Wx::TextCtrl)
+          @txt.set_insertion_point(0)
+          @txt.show_position(0)
+        end
     end
     
     def set_overview(name, text)
@@ -466,6 +488,7 @@ class WxRubyDemo < Wx::Frame
     end
     
     def on_find(event)
+        # FIXME: These problems are probably resolved now
         # Problems
         # 1) For some reason, the control is not properly counting EOL characters - it almost seems to be skipping them
         #    causing the control to incorrectly set the initial insertion point for the the "found" text
@@ -476,47 +499,72 @@ class WxRubyDemo < Wx::Frame
         # Example - When I try to find "menu", the text that gets highlighted is the newline on the second line and the "ME" of 
         #           MENU_EXIT - a total of only 3 characters, when it should highlight all of "menu".  I don't fully understand
         #           this problem, but it doesn't appear until after you attempt to find text beyond the first line :(
-        @nb.set_selection(1)
-        myEnd = @txt.get_last_position()
-        write_text("Last position = %d" % myEnd)
-        textstring = @txt.get_range(0,myEnd).downcase()
-        if Wx::RUBY_PLATFORM == "WXMSW"
-            textstring.sub!(/\n/, "\r\n")
-        end
-        #write_text("textstring - %s" % textstring)
-        start = @txt.get_insertion_point()
-        write_text("insertion point = " + start.to_s())
-        findstring = @finddata.get_find_string().downcase()
-        write_text("finddata = " + findstring)
-        loc = textstring.index(findstring, start)
-        write_text("location = " + loc.to_s())
-        write_text("findstring.length() = " + findstring.length().to_s())
-        if loc == nil and start != 0
+        if @txt.is_a?(Wx::TextCtrl)
+          @nb.set_selection(1)
+          myEnd = @txt.get_last_position()
+          write_text("Last position = %d" % myEnd)
+          textstring = @txt.get_range(0,myEnd).downcase()
+          if Wx::PLATFORM == "WXMSW"
+              textstring.sub!(/\n/, "\r\n")
+          end
+          #write_text("textstring - %s" % textstring)
+          start = @txt.get_insertion_point()
+          write_text("insertion point = " + start.to_s())
+          findstring = @finddata.get_find_string().downcase()
+          write_text("finddata = " + findstring)
+          loc = textstring.index(findstring, start)
+          write_text("location = " + loc.to_s())
+          write_text("findstring.length() = " + findstring.length().to_s())
+          if loc == nil and start != 0
             # string not found, start at beginning
             start = 0
             loc = textstring.index(findstring, start)
-        end
+          end
         
-        if loc == nil
+          if loc == nil
             dlg = Wx::MessageDialog.new(self, 'Find String Not Found', 'Find String Not Found in Demo File',
                                           Wx::OK | Wx::ICON_INFORMATION)
             dlg.show_modal()
             dlg.destroy()
-        end
+          end
         
-        if @finddlg
+          if @finddlg
             if loc == nil
                 @finddlg.set_focus()
                 return
             else
                 @finddlg.destroy()
             end
+          end
+          @txt.show_position(loc)
+          @txt.set_selection(loc, loc + findstring.length())
+        else # Scintilla
+          # FIXME: Take into account search direction
+          @txt.set_target_start(@txt.get_target_end)
+          @txt.set_target_end(@txt.get_length - 1)
+          # FIXME: get flags from find dialog
+          @txt.set_search_flags(0)
+          loc = @txt.search_in_target(event.get_find_string)
+          if loc == -1
+            dlg = Wx::MessageDialog.new(self, 'Find String Not Found', 'Find String Not Found in Demo File',
+                                          Wx::OK | Wx::ICON_INFORMATION)
+            dlg.show_modal()
+            dlg.destroy()
+          end
+
+          if @finddlg
+            if loc == -1
+              @finddlg.set_focus()
+              return
+            end
+          end
+          @txt.ensure_caret_visible
+          @txt.set_selection(loc, loc + event.get_find_string.length)
         end
-        @txt.show_position(loc)
-        @txt.set_selection(loc, loc + findstring.length())
     end
     
     def on_find_next(event)
+        # FIXME: Saving an event is probably a bad idea
         if @finddata.get_find_string() != ""
             on_find(event)
         else
@@ -551,6 +599,125 @@ class WxRubyDemo < Wx::Frame
     def on_maximize(event)
         Wx::log_message("on_maximize")
         event.skip
+    end
+
+    # FIXME: Refactor into a class
+    def setup_editor(parent)
+      panel = Wx::Panel.new(parent)
+      panel.hide
+      sci = setup_scintilla(panel)
+      @save_button = Wx::Button.new(panel, -1, 'Save Changes')
+      @delete_button = Wx::Button.new(panel, -1, 'Delete Modified')
+      @save_button.enable(false);
+      panel.evt_button(@save_button.get_id) { on_save }
+      panel.evt_button(@delete_button.get_id) { on_delete }
+      panel.evt_sci_modified { on_modified }
+
+      radio_buttons = {:original => Wx::RadioButton.new(panel, -1, "Original", Wx::DEFAULT_POSITION, Wx::DEFAULT_SIZE, Wx::RB_GROUP),
+                       :modified => Wx::RadioButton.new(panel, -1, "Modified")}
+      
+      @control_box = Wx::BoxSizer.new(Wx::HORIZONTAL)
+      @control_box.add(Wx::StaticText.new(panel, -1, 'Active Version:'), 0, Wx::RIGHT | Wx::LEFT | Wx::ALIGN_CENTER_VERTICAL, 5)
+      radio_buttons.each_pair { |id, button|
+        @control_box.add(button, 0, Wx::EXPAND | Wx::RIGHT, 5)
+        evt_radiobutton(button.get_id) { |event| on_radio_button(event) }
+      }
+      @control_box.add(@save_button, 0, Wx::RIGHT, 5)
+      @control_box.add(@delete_button, 0)
+
+      @box = Wx::BoxSizer.new(Wx::VERTICAL)
+      @box.add(@control_box, 0, Wx::EXPAND)
+      @box.add(Wx::StaticLine.new(panel), 0, Wx::EXPAND)
+      
+      @box.add(sci, 1, Wx::EXPAND)
+      @box.fit(panel)
+      panel.set_sizer(@box)
+      [panel, sci]
+    end
+
+    def setup_scintilla(parent)
+      sci = Wx::Scintilla.new(parent)
+      font = Wx::Font.new(10, Wx::TELETYPE, Wx::NORMAL, Wx::NORMAL)
+      sci.style_set_font(Wx::SCI_STYLE_DEFAULT, font);
+      sci.set_edge_mode(Wx::SCI_EDGE_LINE)
+      line_num_margin = sci.text_width(Wx::SCI_STYLE_LINENUMBER, "_99999")
+      sci.set_margin_width(0, line_num_margin)
+
+      sci.style_set_foreground(Wx::SCI_STYLE_DEFAULT, Wx::BLACK);
+      sci.style_set_background(Wx::SCI_STYLE_DEFAULT, Wx::WHITE);
+      sci.style_set_foreground(Wx::SCI_STYLE_LINENUMBER, Wx::LIGHT_GREY);
+      sci.style_set_background(Wx::SCI_STYLE_LINENUMBER, Wx::WHITE);
+      sci.style_set_foreground(Wx::SCI_STYLE_INDENTGUIDE, Wx::LIGHT_GREY);
+
+      sci.set_tab_width(2)
+      sci.set_use_tabs(false)
+      sci.set_tab_indents(true)
+      sci.set_back_space_un_indents(true)
+      sci.set_indent(2)
+      sci.set_edge_column(80)
+
+      sci.set_lexer(Wx::SCI_LEX_RUBY)
+      sci.style_clear_all
+      sci.style_set_foreground(2, Wx::RED)
+      sci.style_set_foreground(3, Wx::GREEN)
+      sci.style_set_foreground(5, Wx::BLUE)
+      sci.style_set_foreground(6, Wx::BLUE)
+      sci.style_set_foreground(7, Wx::BLUE)
+      sci.set_key_words(0, "begin break elsif module retry unless end case next return until class ensure nil self when def false not super while alias defined? for or then yield and do if redo true else in rescue undef")
+
+      sci.set_property("fold","1")
+      sci.set_property("fold.compact", "0")
+      sci.set_property("fold.comment", "1")
+      sci.set_property("fold.preprocessor", "1")
+
+      sci.set_margin_width(1, 0)
+      sci.set_margin_type(1, Wx::SCI_MARGIN_SYMBOL)
+      sci.set_margin_mask(1, Wx::SCI_MASK_FOLDERS)
+      sci.set_margin_width(1, 20)
+
+      sci.marker_define(Wx::SCI_MARKNUM_FOLDER, Wx::SCI_MARK_PLUS)
+      sci.marker_define(Wx::SCI_MARKNUM_FOLDEROPEN, Wx::SCI_MARK_MINUS)
+      sci.marker_define(Wx::SCI_MARKNUM_FOLDEREND, Wx::SCI_MARK_EMPTY)
+      sci.marker_define(Wx::SCI_MARKNUM_FOLDERMIDTAIL, Wx::SCI_MARK_EMPTY)
+      sci.marker_define(Wx::SCI_MARKNUM_FOLDEROPENMID, Wx::SCI_MARK_EMPTY)
+      sci.marker_define(Wx::SCI_MARKNUM_FOLDERSUB, Wx::SCI_MARK_EMPTY)
+      sci.marker_define(Wx::SCI_MARKNUM_FOLDERTAIL, Wx::SCI_MARK_EMPTY)
+      sci.set_fold_flags(16)
+
+      sci.set_margin_sensitive(1,1)
+      sci
+    end
+
+    def on_save
+      if not FileTest.directory?(File.dirname(modified_filename))
+        if not Dir.mkdir(File.dirname(modified_filename))
+          Wx::log_message("Unable to create directory 'modified'")
+          return
+        end
+      end
+      File.open(modified_filename, "w") { |file|
+        file.write(@txt.get_text)
+      }
+      # FIXME: Reload module we just saved
+    end
+
+    def on_delete
+      if File.exists?(modified_filename)
+        File.unlink(modified_filename)
+      end
+      # FIXME: Update state
+    end
+
+    def on_modified
+      @save_button.enable(@txt.get_modify)
+    end
+
+    def on_radio_button(event)
+      # FIXME: Can't do anything here yet
+    end
+
+    def modified_filename
+      File.join(File.dirname(@filename), "modified", File.basename(@filename))
     end
 end
 
