705e8b10bc44e34936de88b2ac6ca3822ddad157
Roy Sutton <roys@mindspring.com>
2006-10-14 07:56:49 +0000

    Fixed run.rb to work with more (all?) samples

--- wxruby2/samples/bigdemo/run.rb
+++ wxruby2/samples/bigdemo/run.rb
@@ -20,6 +20,30 @@ class Log
   alias write write_text
 end
 
+class RunFrame < Wx::Frame
+  attr_accessor :otherWin
+
+  def initialize(sample)
+    super(nil, -1, "wxRuby Demo: #{sample}",
+                             Wx::Point.new(50, 50), 
+                             Wx::Size.new(450, 340))
+
+    create_status_bar
+    
+    menu_file = Wx::Menu.new()
+    menu_file.append(Wx::ID_EXIT, "E&xit\tAlt-X", "Quit this program")
+    menu_bar = Wx::MenuBar.new()
+    menu_bar.append(menu_file, "&File")
+    evt_menu(Wx::ID_EXIT) { close }
+
+    set_menu_bar(menu_bar)
+
+    # TODO:  Some samples may need clean-up calls?
+    evt_close {|evt| evt.skip }
+  end
+
+end
+
 # Wx::App is the container class for any wxruby app - only a single
 # instance is required
 class MinimalApp < Wx::App
@@ -29,28 +53,12 @@ class MinimalApp < Wx::App
   end
 
   def on_init
-    frame = Wx::Frame.new(nil, -1, "wxRuby Demo: #{@sample}",
-                             Wx::Point.new(50, 50), 
-                             Wx::Size.new(450, 340))
-
-    frame.create_status_bar
-    
-    menu_file = Wx::Menu.new()
-    menu_file.append(Wx::ID_EXIT, "E&xit\tAlt-X", "Quit this program")
-    menu_bar = Wx::MenuBar.new()
-    menu_bar.append(menu_file, "&File")
-    evt_menu(Wx::ID_EXIT) { frame.close }
-
-    frame.set_menu_bar(menu_bar)
+    frame = RunFrame.new(@sample)
     frame.show
-
-    # TODO:  Some samples may need clean-up calls?
-    frame.evt_close {|evt| evt.skip }
-
     win = Demo.run(frame, frame, Log.new)
     # a window will be returned if the demo does not create
     # its own top-level window
-    if win
+    if win.class.ancestors.include?(Wx::Window)
       frame.set_size(640, 480)
       win.set_focus
     else
