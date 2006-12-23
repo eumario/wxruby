769ea79ec8cf97d194f6fc4a69d7ecdaee23fd91
Alex Fenton <alex@pressure.to>
2006-12-23 03:04:53 +0000

    Fix get_ruby_object so it works with SWIG tracking, move find_window_xx instance methods into ruby

--- wxruby2/lib/wx/classes/window.rb
+++ wxruby2/lib/wx/classes/window.rb
@@ -2,8 +2,30 @@
 # released under the MIT-style wxruby2 license
 
 class Wx::Window
-  # create a DC for drawing on the window, run the block passing in the DC
+  # Creates a device context which can be used for drawing on the
+  # window, and passes it into a block. Note that this method should
+  # only be used within an on_paint handler.
   def paint
     yield Wx::PaintDC.new(self)
+  end
+
+  # Recursively searches all windows below +self+ and returns the first
+  # window which has the id +an_id+. This corresponds to the find_window
+  # method method in WxWidgets when called with an integer.
+  def find_window_by_id(an_id)
+    Wx::Window.find_window_by_id(an_id, self)
+  end
+
+  # Searches all windows below +self+ and returns the first window which
+  # has the name +a_name+ This corresponds to the find_window method method
+  # in WxWidgets when called with an string.
+  def find_window_by_name(a_name)
+    Wx::Window.find_window_by_name(a_name, self)
+  end
+
+  # Searches all windows below +self+ and returns the first window which
+  # has the label +a_label+.
+  def find_window_by_label(a_label)
+    Wx:Window.find_window_by_label(a_label, self)
   end
 end
