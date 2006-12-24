734b2f5f5c01a28263e6d3e7f8d0d948637ef39b
Alex Fenton <alex@pressure.to>
2006-12-24 12:03:48 +0000

    Add support for MenuEvent (MENU_OPEN, MENU_CLOSE, MENU_HIGHLIGHT)

--- swig/classes/include/wxMenuEvent.h
+++ swig/classes/include/wxMenuEvent.h
@@ -1,35 +1,15 @@
-// wxMenuEvent.h
-// This file was automatically generated
-// by extractxml.rb, part of the wxRuby project
-// Do not make changes directly to this file!
+// Copyright 2004-2006 by Kevin Smith
+// released under the MIT-style wxruby2 license
 
 #if !defined(_wxMenuEvent_h_)
 #define _wxMenuEvent_h_
 class wxMenuEvent : public wxEvent
 {
 public:
-	/**
-	 * \brief Constructor. 
-	 * \param WXTYPE   
-	 * \param int   
-	 * \param wxDC*   
-	*/
-
-   wxMenuEvent(WXTYPE  id = 0, int  id = 0, wxDC*  dc = NULL) ;
-	/**
-	 * \brief Returns the menu identifier associated with the event. This method should be
-only used with the   events. 
-	*/
-
-  int GetMenuId() const;
-	/**
-	 * \brief Returns   if the menu which is being opened or closed is a popup menu, 
-  if it is a normal one.
-
-This method should be only used with the   and   events. 
-	*/
-
-  bool IsPopup() const;
+   wxMenuEvent(int id = 0, int menu_id = 0, wxMenu* menu = NULL);
+   wxMenu* GetMenu() const;
+   int GetMenuId() const;
+   bool IsPopup() const;
 };
 
 
