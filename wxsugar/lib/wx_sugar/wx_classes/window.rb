a5f0680909fce13c1e4ca4c234c065d85d749292
Alex Fenton <alex@pressure.to>
2006-12-30 16:50:02 +0000

    Added find_window and descendants methods

--- wxsugar/lib/wx_sugar/wx_classes/window.rb
+++ wxsugar/lib/wx_sugar/wx_classes/window.rb
@@ -1,5 +1,26 @@
 # extensions to Wx::Window
 class Wx::Window
+  # Returns an array containing this window and all windows descended
+  # from it.
+  def get_descendants
+    get_children.inject([ self ]) do | desc, child |
+      desc + child.get_descendants
+    end
+  end
+
+  # Passed a block, returns the first window within self's window
+  # hierarchy for which the block evaluates to true. A class name may
+  # optionally be passed to restrict the search to windows of that class
+  # (or its subclasses) only - this can be useful if the search
+  # specification calls methods that are only defined for certain
+  # classes.
+  #
+  #  # Example - find a StaticText with a label matching /foo/
+  #  a_frame.find_window(Wx::StaticText) { | tx | tx.label =~ /foo/ }
+  def find_window(a_class = Wx::Window, &block)
+    descendants.grep(a_class).detect(&block)
+  end
+
   # Returns the window's current size as a two element array
   def get_size_xy
     size = get_size
