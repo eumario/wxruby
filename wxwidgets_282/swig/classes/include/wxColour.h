3f8a0262b4502e94e66ae684085101f48fc2fc5d
Alex Fenton <alex@pressure.to>
2007-03-21 10:20:07 +0000

    Changes in Wx::Colour API 2.6 -> 2.8

--- wxwidgets_282/swig/classes/include/wxColour.h
+++ wxwidgets_282/swig/classes/include/wxColour.h
@@ -1,6 +1,5 @@
-// wxColour.h
-// This file was automatically generated
-// by extractxml.rb, part of the wxRuby project
+// Copyright 2007 by Kevin Smith
+// released under the MIT-style wxruby2 license
 // Do not make changes directly to this file!
 
 #if !defined(_wxColour_h_)
@@ -8,67 +7,17 @@
 class wxColour : public wxObject
 {
 public:
-	/**
-	 * \brief Default constructor. 
-	*/
-
    wxColour() ;
-	/**
-	 * \brief Constructs a colour from red, green and blue values. 
-	 * \param const unsigned char  
-	 * \param const unsigned char  
-	 * \param const unsigned char  
-	*/
-
-   wxColour(const unsigned char  red , const unsigned char  green , const unsigned char  blue ) ;
-	/**
-	 * \brief Constructs a colour object using a colour name
-listed in  . 
-	 * \param const wxString&   
-	*/
-
+   wxColour(const unsigned char  red , const unsigned char  green , const unsigned char  blue, const unsigned char alpha = wxALPHA_OPAQUE );
    wxColour(const wxString&  colourNname ) ;
-	/**
-	 * \brief  
-	 * \param const wxColour&  
-	*/
-
    wxColour(const wxColour&  colour ) ;
-	/**
-	 * \brief Returns the blue intensity. 
-	*/
-
+  unsigned char Alpha() const;
   unsigned char Blue() const;
-	/**
-	 * \brief Returns a pixel value which is platform-dependent. On Windows, a COLORREF is returned.
-On X, an allocated pixel value is returned.
-
--1 is returned if the pixel is invalid (on X, unallocated). 
-	*/
-
+  wxString GetAsString(long flags) const;
   long GetPixel() const;
-	/**
-	 * \brief Returns the green intensity. 
-	*/
-
   unsigned char Green() const;
-	/**
-	 * \brief Returns true if the colour object is valid (the colour has been initialised with RGB values). 
-	*/
-
-  bool Ok() const;
-	/**
-	 * \brief Returns the red intensity. 
-	*/
-
+  bool IsOk() const;
   unsigned char Red() const;
-	/**
-	 * \brief Sets the RGB intensity values. 
-	 * \param const unsigned char  
-	 * \param const unsigned char  
-	 * \param const unsigned char  
-	*/
-
   void Set(const unsigned char  red , const unsigned char  green , const unsigned char  blue ) ;
 };
 
