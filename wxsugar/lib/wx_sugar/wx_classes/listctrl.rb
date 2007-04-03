6f64f90a352110e84690a22d7aa801efb73566c0
Alex Fenton <alex@pressure.to>
2007-04-03 18:45:26 +0000

    Implement find_string method cognate with ControlWithItems

--- wxsugar/lib/wx_sugar/wx_classes/listctrl.rb
+++ wxsugar/lib/wx_sugar/wx_classes/listctrl.rb
@@ -2,13 +2,35 @@ require 'wx_sugar/enumerable_controls'
 
 class Wx::ListCtrl
   include WxSugar::EnumerableControl
-  collection :strings, StringsCollection
+  collection :strings, ListItemTextCollection
   collection :data, ItemDataCollection
 
   alias :get_string :get_item_text
   alias :set_string :set_item_text
+  alias :get_count :get_item_count
 
+  # Appends 
   def <<(str)
     insert_item(get_item_count, str)
+  end
+
+  # Emulate the ControlWithItems find_string method.
+  def find_string(str, case_sensitive = false)
+    if case_sensitive
+      find_string_sensitively(str)
+    else
+      find_item(-1, str)
+    end
+  end
+
+  private
+  # Implement a case-sensitive search for ListCtrl (default find_item
+  # with string is always case-insensitive)
+  def find_string_sensitively(str)
+    start = -1
+    until found = find_item(start, str) and get_item_text(found) == str
+      start = found + 1
+    end
+    return found
   end
 end
