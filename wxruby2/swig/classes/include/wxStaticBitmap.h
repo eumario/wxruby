22a63d1ceb9d241e7b9b470bbc1361837d497c8b
Alex Fenton <alex@pressure.to>
2007-07-04 19:00:47 +0000

    Added 2.8 methods to StaticBitmap & document them; cleanup header file

--- wxruby2/swig/classes/include/wxStaticBitmap.h
+++ wxruby2/swig/classes/include/wxStaticBitmap.h
@@ -1,53 +1,30 @@
-// wxStaticBitmap.h
-// This file was automatically generated
-// by extractxml.rb, part of the wxRuby project
-// Do not make changes directly to this file!
+// Copyright 2006 - 2007 by Kevin Smith
+// released under the MIT-style wxruby2 license
 
 #if !defined(_wxStaticBitmap_h_)
 #define _wxStaticBitmap_h_
 class wxStaticBitmap : public wxControl
 {
 public:
-	/**
-	 * \brief Default constructor. 
-	*/
-
    wxStaticBitmap() ;
-	/**
-	 * \brief Constructor, creating and showing a text control. 
-	 * \param wxWindow*   
-	 * \param wxWindowID  
-	 * \param const wxBitmap&   
-	 * \param const wxPoint&   
-	 * \param const wxSize&   
-	 * \param long  
-	 * \param const wxString&   
-	*/
-
-   wxStaticBitmap(wxWindow*  parent , wxWindowID  id , const wxBitmap&  label , const wxPoint&  pos , const wxSize&  size = wxDefaultSize, long style = 0, const wxString&  name = wxT("staticBitmap")) ;
-	/**
-	 * \brief Creation function, for two-step construction. For details see  . 
-	 * \param wxWindow*   
-	 * \param wxWindowID  
-	 * \param const wxBitmap&   
-	 * \param const wxPoint&   
-	 * \param const wxSize&   
-	 * \param long  
-	 * \param const wxString&   
-	*/
-
-  bool Create(wxWindow*  parent , wxWindowID  id , const wxBitmap&  label , const wxPoint&  pos , const wxSize&  size = wxDefaultSize, long style = 0, const wxString&  name = wxT("staticBitmap")) ;
-	/**
-	 * \brief Returns a reference to the label bitmap. 
-	*/
-
+   wxStaticBitmap(wxWindow*  parent, 
+				  wxWindowID  id, 
+				  const wxBitmap&  label, 
+				  const wxPoint&  pos = wxDefaultPosition, 
+				  const wxSize&  size = wxDefaultSize, 
+				  long style = 0, 
+				  const wxString&  name = wxT("staticBitmap"));
+   bool Create(wxWindow*  parent , 
+			   wxWindowID  id, 
+			   const wxBitmap&  label, 
+			   const wxPoint&  pos = wxDefaultPosition, 
+			   const wxSize&  size = wxDefaultSize, 
+			   long style = 0, 
+			   const wxString&  name = wxT("staticBitmap"));
   wxBitmap GetBitmap() const;
-	/**
-	 * \brief Sets the bitmap label. 
-	 * \param const wxBitmap&   
-	*/
-
-  virtual void SetBitmap(const wxBitmap&   label ) ;
+  wxIcon GetIcon() const;
+  virtual void SetBitmap(const wxBitmap& label);
+  virtual void SetIcon(const wxIcon& label);
 };
 
 
