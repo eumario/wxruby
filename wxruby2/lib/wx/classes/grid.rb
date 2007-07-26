0ce00d4c07b7dc5f9c55b416197385df6b3c97df
Alex Fenton <alex@pressure.to>
2007-07-26 09:02:40 +0000

    Try once more to do all GC memory management in ruby with revised cell-level functions; remove now-unneeded GC guards

--- wxruby2/lib/wx/classes/grid.rb
+++ wxruby2/lib/wx/classes/grid.rb
@@ -10,20 +10,52 @@ class Wx::Grid
   # directors we cannot allow the ruby object they are originally
   # associated with to be swept by GC.
   #
-  # For the default Grid editors and renderers, and for those attached
-  # to individual cells, marking is done in C++ in the SWIG wrapping, in
-  # swig/classes/Grid.i. However, because of deficiencies in the
-  # wxWidgets API, it seems the only way to deal with renderers and
-  # editors associated with columns and rows is to handle them in Ruby.
-  # The code below stores Ruby references to the editors and renderers
-  # so they are picked up when GC sweeps.
+  # The code below stores Ruby redefines all methods that may
+  # potentially set an Editor or Renderer, and stores a reference to
+  # it in an instance variable, so they are not disposed of up when GC sweeps.
   wx_init = self.instance_method(:initialize)
   define_method(:initialize) do | *args |
     wx_init.bind(self).call(*args)
+    @__default_editor   = nil
+    @__default_renderer = nil
+
+    @__cell_editors   = []
+    @__cell_renderers = []
+
     @__col_renderers = []
     @__col_editors   = []
     @__row_renderers = []
     @__row_editors   = []
+  end
+  
+  # store default editor
+  wx_set_default_editor = self.instance_method(:set_default_editor)
+  define_method(:set_default_editor) do | editr |
+    wx_set_default_editor.bind(self).call(editr)
+    @__default_editor = editr
+  end
+
+  # store default renderer
+  wx_set_default_renderer = self.instance_method(:set_default_renderer)
+  define_method(:set_default_renderer) do | rendr |
+    wx_set_default_renderer.bind(self).call(rendr)
+    @__default_renderer = rendr
+   end
+
+  # store cell editors
+  wx_set_cell_editor = self.instance_method(:set_cell_editor)
+  define_method(:set_cell_editor) do | row, col, editr |
+    wx_set_cell_editor.bind(self).call(row, col, editr)
+    @__cell_editors[row] ||= []
+    @__cell_editors[row][col] = editr
+   end
+
+  # store cell renderer
+  wx_set_cell_renderer = self.instance_method(:set_cell_renderer)
+  define_method(:set_cell_renderer) do | row, col, rendr |
+    wx_set_cell_renderer.bind(self).call(row, col, rendr)
+    @__cell_renderers[row] ||= []
+    @__cell_renderers[row][col] = rendr
   end
 
   wx_set_col_attr = self.instance_method(:set_col_attr)
@@ -54,37 +86,45 @@ class Wx::Grid
   # and rows with stored editors and renderers may move.
   wx_insert_rows = self.instance_method(:insert_rows)
   define_method(:insert_rows) do | pos, num |
-    gc_was_disabled = GC.disable
     wx_insert_rows.bind(self).call(pos, num)
     num.times { @__row_editors.insert(pos, nil) }
     num.times { @__row_renderers.insert(pos, nil) }
-    GC.enable unless gc_was_disabled
+    num.times { @__cell_editors.insert(pos, []) }
+    num.times { @__cell_renderers.insert(pos, []) }
   end
 
   wx_insert_cols = self.instance_method(:insert_cols)
   define_method(:insert_cols) do | pos, num |
-    gc_was_disabled = GC.disable
     wx_insert_cols.bind(self).call(pos, num)
     num.times { @__col_editors.insert(pos, nil) }
     num.times { @__col_renderers.insert(pos, nil) }
-    GC.enable unless gc_was_disabled
+    num.times do
+      @__cell_editors.map { | col | col.insert(pos, []) if col }
+    end
+    num.times do
+      @__cell_renderers.map { | col | col.insert(pos, []) if col }
+    end
   end
 
   wx_delete_rows = self.instance_method(:delete_rows)
   define_method(:delete_rows) do | pos, num |
-    gc_was_disabled = GC.disable
     wx_delete_rows.bind(self).call(pos, num)
     @__row_editors.slice!(pos, num)
     @__row_renderers.slice!(pos, num)
-    GC.enable unless gc_was_disabled
+    @__cell_editors.slice!(pos, num)
+    @__cell_renderers.slice!(pos, num)
   end
  
   wx_delete_cols = self.instance_method(:delete_cols)
   define_method(:delete_cols) do | pos, num |
-    gc_was_disabled = GC.disable
     wx_delete_cols.bind(self).call(pos, num)
     @__col_editors.slice!(pos, num)
     @__col_renderers.slice!(pos, num)
-    GC.enable unless gc_was_disabled
+    num.times do
+      @__cell_editors.map { | col | col.slice!(pos, num) if col }
+    end
+    num.times do
+      @__cell_renderers.map { | col | col.slice!(pos, num) if col }
+    end
   end
 end
