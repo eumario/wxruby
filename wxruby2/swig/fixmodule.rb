451d3169bfc3d96e9a078bbfcd55468c7dd2bb9d
Alex Fenton <alex@pressure.to>
2007-08-31 19:22:16 +0000

    Super-ugly fixes to make TreeCtrl#on_compare_items work on MSW

--- wxruby2/swig/fixmodule.rb
+++ wxruby2/swig/fixmodule.rb
@@ -49,6 +49,28 @@ def fixmodule(filename)
         line = result.join("\n")
         found_swig_class = true
       end
+      
+      # Ugly: special fixes for TreeCtrl - these macros and extra funcs
+      # are needed to allow user-defined sorting to work
+      if core_name == "TreeCtrl"
+        # default ctor needed for Swig::Director
+        if line["Director(VALUE self) : swig_self(self), swig_disown_flag(false)"]
+          line = "    Director() { } // added by fixmodule.rb \n" + line
+        end
+        if line["SwigDirector_wxTreeCtrl::SwigDirector_wxTreeCtrl(VALUE self)"]
+          line = "IMPLEMENT_DYNAMIC_CLASS(SwigDirector_wxTreeCtrl,  wxTreeCtrl);\n" + line
+          # We also need to tweak the header file
+          treectrl_h_file = filename.sub(/cpp$/, "h")
+          contents = File.read(treectrl_h_file)
+          contents.sub!(/\};/, "private:
+DECLARE_DYNAMIC_CLASS(SwigDirector_wxTreeCtrl);
+};")
+          contents.sub!(/public:/, "public:
+    SwigDirector_wxTreeCtrl() {} ;")
+
+          File.open(treectrl_h_file, 'w') { | f | f.write(contents) }
+        end
+      end # end horrible TreeCtrl fixes
 
       # comment out swig_up because it is defined global in every module
       if(line.index("bool Swig::Director::swig_up"))
