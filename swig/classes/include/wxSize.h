f8d65c84ff20cf730cf9c6260ea151e54840617c
Alex Fenton <alex@pressure.to>
2007-09-06 21:55:54 +0000

    Additional operator support for Point and Size; wx 2.8 Size methods

--- swig/classes/include/wxSize.h
+++ swig/classes/include/wxSize.h
@@ -9,9 +9,21 @@ class wxSize
 public:
    wxSize();
    wxSize(int  width, int  height);
+  void DecBy(int dx, int dy);
+  void DecBy(const wxSize& sz);
+  void DecBy(int d);
+  void DecTo(const wxSize& sz);
   int GetWidth() const;
-  int GetHeight() const;
+  int GetHeight() const;    
+  int GetX();
+  int GetY();
+  void IncBy(int dx, int dy);
+  void IncBy(const wxSize& sz);
+  void IncBy(int d);
+  void IncTo(const wxSize& sz);
+  bool IsFullySpecified() const;
   void Set(int  width, int  height);
+  void SetDefaults(const wxSize& size);
   void SetHeight(int height );
   void SetWidth(int width );
 };
