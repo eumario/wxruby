1db020ed6ca4d4f9a198f68103bd80563e8e8e3e
Alex Fenton <alex@pressure.to>
2007-09-10 18:23:15 +0000

    Remove features from wxSugar now merged into wxRuby core

--- wxsugar/lib/wx_sugar/all.rb
+++ wxsugar/lib/wx_sugar/all.rb
@@ -1,5 +1,5 @@
-%w[ accessors delayed_constructors event_connector 
-    keyword_constructors layout menu wx_classes ].each do | ext_feature |
+%w[ delayed_constructors event_connector 
+    layout menu wx_classes ].each do | ext_feature |
   require 'wx_sugar/' + ext_feature    
 end 
 
