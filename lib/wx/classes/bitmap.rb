624e88a1558ad681bfdd711d02f7a725c56d4522
Alex Fenton <alex@pressure.to>
2007-10-08 08:42:52 +0000

    Fix namespace error in Bitmap#draw

--- lib/wx/classes/bitmap.rb
+++ lib/wx/classes/bitmap.rb
@@ -15,9 +15,9 @@ class Wx::Bitmap
   # Accepts a block, which will be passed a device context which can be
   # used to draw upon the Bitmap
   def draw
-    dc = MemoryDC.new
+    dc = Wx::MemoryDC.new
     dc.select_object(self)
     yield dc
-    dc.select_object( NULL_BITMAP )
+    dc.select_object( Wx::NULL_BITMAP )
   end
 end
