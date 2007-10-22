d4536ee6f38ae604cdb2f3c71ec9b71d87895a0f
Alex Fenton <alex@pressure.to>
2007-10-22 16:37:35 +0000

    Ruby-ify syntax in wizard sample

--- samples/etc/wizard.rb
+++ samples/etc/wizard.rb
@@ -14,42 +14,41 @@ rescue LoadError => no_wx_err
 end
 
 class MyFrame < Wx::Frame
-  def initialize(title, pos, size, style = Wx::DEFAULT_FRAME_STYLE)
-    super(nil, -1, title, pos, size, style)
+  def initialize(title, pos, size)
+    super(nil, :title => title, :position => pos, :size => size)
 
-
-    menuFile = Wx::Menu.new()
-    helpMenu = Wx::Menu.new()
+    menuFile = Wx::Menu.new
+    helpMenu = Wx::Menu.new
     helpMenu.append(Wx::ID_ABOUT, "&About...\tF1", "Show about dialog")
     menuFile.append(Wx::ID_EXIT, "E&xit\tAlt-X", "Quit this program")
-    menuBar = Wx::MenuBar.new()
+    menuBar = Wx::MenuBar.new
     menuBar.append(menuFile, "&File")
     menuBar.append(helpMenu, "&Help")
-    set_menu_bar(menuBar)
+    self.menu_bar = menuBar
 
-    create_status_bar(2)
-    set_status_text("Welcome to wxRuby!")
-    s = Wx::StaticText.new(self, -1, 'The Wizard has completed')
+    create_status_bar(1)
+    self.status_text = "Welcome to wxRuby!"
+    s = Wx::StaticText.new(self, :label => 'The Wizard has completed')
+    evt_menu Wx::ID_EXIT, :on_quit
+    evt_menu Wx::ID_ABOUT, :on_about
 
-
-    evt_menu(Wx::ID_EXIT) { on_quit }
-    evt_menu(Wx::ID_ABOUT) { on_about }
-    w = Wx::Wizard.new(self, -1, 'The WxRuby Wizard')
+    w = Wx::Wizard.new(self, :title => 'The WxRuby Wizard')
     p1 = Wx::WizardPageSimple.new(w)
-    s = Wx::StaticText.new(p1, -1, 'This is the first page')
+    s = Wx::StaticText.new(p1, :label => 'This is the first page')
+
     p2 = Wx::WizardPageSimple.new(w, p1)
     p1.set_next(p2)
-    s = Wx::StaticText.new(p2, -1, 'This is the second page')
+    s = Wx::StaticText.new(p2, :label => 'This is the second page')
+
     p3 = Wx::WizardPageSimple.new(w, p2)
     p2.set_next(p3)
-    s = Wx::StaticText.new(p3, -1, 'This is the final page')
+    s = Wx::StaticText.new(p3, :label => 'This is the final page')
 
-    evt_wizard_page_changed(w.get_id) { p "page changed" }
-    evt_wizard_page_changing(w.get_id) { p "page changing" }
-    evt_wizard_help(w.get_id) { p "wizard help" }
-    evt_wizard_cancel(w.get_id) { p "wizard cancelled" }
-    evt_wizard_finished(w.get_id) { p "wizard finished" }
-
+    evt_wizard_page_changed(w) { p "page changed" }
+    evt_wizard_page_changing(w) { p "page changing" }
+    evt_wizard_help(w) { p "wizard help" }
+    evt_wizard_cancel(w) { p "wizard cancelled" }
+    evt_wizard_finished(w) { p "wizard finished" }
 
     w.run_wizard(p1)
   end
@@ -70,7 +69,6 @@ class RbApp < Wx::App
     frame = MyFrame.new("Wizard wxRuby App",
                         Wx::Point.new(50, 50), 
                         Wx::Size.new(450, 340))
-
     frame.show(true)
 
   end
