e50170b094094fbbfa66d089e0ae0b2172507355
Alex Fenton <alex@pressure.to>
2007-10-23 16:53:57 +0000

    Added Ruby-based support for item_data in CheckListBox (Chauk-Mean P)

--- lib/wx/classes/checklistbox.rb
+++ lib/wx/classes/checklistbox.rb
@@ -1,30 +1,44 @@
-# Display a listbox with a checkbox for each item
-class Wx::CheckListBox < Wx::ListBox
-
-  # According to the wxWidgets documentation: "wxCheckListBox uses
-  # client data in its implementation, and therefore this is not
-  # available to the application."   
-  #
-  # So, rather than crashing, raise an Exception if this is attempted
-  def append(*args)
-    if args.length == 2 
-      Kernel.raise "Cannot use item data with Wx::CheckListBox"
-    else 
-      super
-    end
+# A ListBox, where each item has a checkbox next to it
+class Wx::CheckListBox
+  # wxRuby redefinitions of core methods to add item data support (not
+  # available in wxWidgets). Plus convenience get_checked_items method.
+  wx_init = self.instance_method(:initialize)
+  define_method(:initialize) do | *args |
+    @__wx_item_data = []
+    wx_init.bind(self).call(*args)
+  end
+  
+  # Call method in ControlWithItems, then sync item data
+  def append(item, data = nil)
+    super(item)
+    @__wx_item_data << data
   end
 
-  # As above
-  def insert(*args)
-    if args.length == 3
-      Kernel.raise "Cannot use item data with Wx::CheckListBox"
-    else 
-      super
-    end
+  # Call method in ControlWithItems, then sync item data
+  def clear
+    super
+    @__wx_item_data.clear
   end
 
-  # As above
-  def set_item_data(index, data)
-    Kernel.raise "Cannot use item data with Wx::CheckListBox"
+  # delete is defined in Wx::CheckListBox, so must be called in this class
+  wx_delete = self.instance_method(:delete)
+  define_method(:delete) do | n |
+    wx_delete.bind(self).call(n)
+    @__wx_item_data.delete_at(n)
+  end
+
+  # Returns an array of indices of items that are currently checked.
+  def get_checked_items
+    find_all { | i | is_checked(i) }    
+  end
+  
+  # Wholly redefined here
+  def get_item_data(n)
+    @__wx_item_data[n]
+  end
+
+  # Wholly redefined here
+  def set_item_data(n, data)
+    @__wx_item_data[n] = data
   end
 end
