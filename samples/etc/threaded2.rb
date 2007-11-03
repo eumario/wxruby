9370df06f6fd0008adba410bbf7a908f4434aea5
Mario Steele <mario@ruby-im.net>
2007-11-03 03:26:59 +0000

    Adding of methods: *  Wx::App#init_threaded_mode() *  Wx::App#shutdown_threaded_mode() *  Wx::App#set_threaded_interval() *  Wx::App#get_threaded_interval() *  Wx::App#threaded_interval= *  Wx::App#threaded_interval *  Wx::App::THREAD_TIMER_ID
    Added new sample for new Threaded methods.

---
+++ samples/etc/threaded2.rb
@@ -0,0 +1,73 @@
+#!/usr/bin/env ruby
+# wxRuby2 Sample Code. Copyright (c) 2004-2006 Kevin B. Smith 
+# Freely reusable code: see SAMPLES-LICENSE.TXT for details
+
+begin
+  require 'wx'
+rescue LoadError => no_wx_err
+  begin
+    require 'rubygems'
+    require 'wx'
+  rescue LoadError
+    raise no_wx_err
+  end
+end
+
+# This simple sample demonstrates how to use Ruby 1.8's lightweight
+# (green) threads to execute non-GUI code in parallel with a wxRuby
+# GUI. This strategy is useful in a number of situations:
+# 
+# * To keep the GUI responsive whilst computationally intensive
+#   operations are carried out in the background
+# * To keep the GUI responsive while waiting for networking operations
+#   to complete 
+# 
+# The basic problem is that, as with other Ruby GUI toolkits, non-GUI
+# threads will not, by default, get allocated time to run while Ruby is
+# busy in Wx code - the main wxRuby event loop. Strategies to deal with
+# this include using non-blocking IO, and, more generically, using
+# wxRuby's Timer class to explicitly allocate time for non-GUI threads
+# to run. The latter technique is shown here.
+
+# This frame shows a set of progress bars which monitor progress of
+# long-running tasks. In this example, this long-running task is
+# emulated by simply sleep-ing for random periods, but could equally be
+# downloading from a socket or parsing a file.
+class ProgressFrame < Wx::Frame
+  STEPS = 20
+  def initialize
+    super(nil, :title => 'Threading demo')
+    @gauges = []
+    panel = Wx::Panel.new(self)
+    sizer = Wx::BoxSizer.new(Wx::VERTICAL)
+    # show ten gauges
+    10.times do 
+      gauge = Wx::Gauge.new(panel, :range => STEPS)
+      # For each gauge, start a new thread in which the task runs
+      Thread.new do 
+        # The long-running task
+        STEPS.times do | i |
+          sleep rand(100) / 50.0
+          # Update the main GUI
+          gauge.value = i + 1
+        end
+      end
+      @gauges << gauge
+      sizer.add(gauge, 0, Wx::GROW|Wx::ALL, 2)
+    end
+    panel.sizer = sizer
+    sizer.fit(panel)
+  end
+end
+
+# This app class creates a frame, and, importantly, a timer to allow
+class GaugeApp < Wx::App
+  def on_init
+    # Initialize the Threaded Mode for this Application
+    init_threaded_mode()
+    prog = ProgressFrame.new
+    prog.show
+  end
+end
+
+GaugeApp.new.main_loop
