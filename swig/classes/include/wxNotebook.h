c09f94d28368985b0eaf114d2523dd2472a8b23f
Alex Fenton <alex@pressure.to>
2007-11-22 22:29:51 +0000

    Header clean-up

--- swig/classes/include/wxNotebook.h
+++ swig/classes/include/wxNotebook.h
@@ -1,191 +1,54 @@
-// wxNotebook.h
-// This file was automatically generated
-// by extractxml.rb, part of the wxRuby project
-// Do not make changes directly to this file!
+// Copyright 2004-2007, wxRuby development team
+// released under the MIT-like wxRuby2 license
+
 
 #if !defined(_wxNotebook_h_)
 #define _wxNotebook_h_
 class wxNotebook : public wxControl
 {
 public:
-	/**
-	 * \brief Default constructor. 
-	*/
-
    wxNotebook() ;
-	/**
-	 * \brief Constructs a notebook control.
-
-Note that sometimes you can reduce flicker by passing the wxCLIP_CHILDREN window style. 
-	 * \param wxWindow*  
-	 * \param wxWindowID   
-	 * \param const wxPoint&  
-	 * \param const wxSize&  
-	 * \param long  
-	 * \param const wxString&   
-	*/
-
-  wxNotebook(wxWindow*  parent , wxWindowID  id , const wxPoint& pos = wxDefaultPosition, const wxSize& size = wxDefaultSize, long style = 0, const wxString&  name = wxT("notebook"));
-	/**
-	 * \brief Destroys the wxNotebook object. 
-	*/
-
+  wxNotebook(wxWindow*  parent , 
+			 wxWindowID  id , 
+			 const wxPoint& pos = wxDefaultPosition, 
+			 const wxSize& size = wxDefaultSize, 
+			 long style = 0, 
+			 const wxString&  name = wxT("notebook"));
   virtual  ~wxNotebook() ;
-	/**
-	 * \brief Adds a new page. 
-	 * \param wxNotebookPage*  
-	 * \param const wxString&  
-	 * \param bool  
-	 * \param int  
-	*/
-
-  bool AddPage(wxNotebookPage*  page , const wxString&  text , bool select = false, int imageId = -1) ;
-	/**
-	 * \brief Cycles through the tabs. 
-	 * \param bool  
-	*/
-
-  void AdvanceSelection(bool forward = true) ;
-	/**
-	 * \brief Sets the image list for the page control and takes ownership of
-the list. 
-	 * \param wxImageList*  
-	*/
-
-  void AssignImageList(wxImageList*  imageList ) ;
-	/**
-	 * \brief Creates a notebook control. See   for a description
-of the parameters. 
-	 * \param wxWindow*  
-	 * \param wxWindowID   
-	 * \param const wxPoint&  
-	 * \param const wxSize&  
-	 * \param long  
-	 * \param const wxString&   
-	*/
-
-  bool Create(wxWindow*  parent , wxWindowID  id , const wxPoint& pos , const wxSize&  size , long style , const wxString&  name = wxT("notebook"));
-	/**
-	 * \brief Deletes all pages. 
-	*/
-
-  bool DeleteAllPages() ;
-	/**
-	 * \brief Deletes the specified page, and the associated window. 
-	 * \param int  
-	*/
-
-  bool DeletePage(int  page ) ;
-	/**
-	 * \brief  
-	*/
-
+  bool AddPage(wxNotebookPage*  page , 
+			   const wxString&  text , 
+			   bool select = false, 
+			   int imageId = -1) ;
+  void AdvanceSelection(bool forward = true);
+  void AssignImageList(wxImageList* imageList);
+  bool Create(wxWindow* parent, 
+			  wxWindowID id, 
+			  const wxPoint& pos, 
+			  const wxSize& size, 
+			  long style,
+			  const wxString&  name = wxT("notebook"));
+  bool DeleteAllPages();
+  bool DeletePage(int  page);
   wxImageList* GetImageList() const;
-	/**
-	 * \brief Returns the window at the given page position. 
-	 * \param int  
-	*/
-
-  wxNotebookPage* GetPage(int  page ) ;
-	/**
-	 * \brief Returns the number of pages in the notebook control. 
-	*/
-
+  wxNotebookPage* GetPage(int page);
   int GetPageCount() const;
-	/**
-	 * \brief Returns the image index for the given page. 
-	 * \param int   
-	*/
-
-  int GetPageImage(int  nPage ) const;
-	/**
-	 * \brief Returns the string for the given page. 
-	 * \param int   
-	*/
-
-  wxString GetPageText(int  nPage ) const;
-	/**
-	 * \brief Returns the number of rows in the notebook control. 
-	*/
-
+  int GetPageImage(int nPage) const;
+  wxString GetPageText(int nPage) const;
   int GetRowCount() const;
-	/**
-	 * \brief Returns the currently selected page, or $-1$ if none was selected.
-
-Note that this method may return either the previously or newly selected page
-when called from the   handler depending on
-the platform and so
-  should be
-used instead in this case. 
-	*/
-
   int GetSelection() const;
-	/**
-	 * \brief Inserts a new page at the specified position. 
-	 * \param int  
-	 * \param wxNotebookPage*  
-	 * \param const wxString&  
-	 * \param bool  
-	 * \param int  
-	*/
-
-  bool InsertPage(int  index , wxNotebookPage*  page , const wxString&  text , bool select = false, int imageId = -1) ;
-	/**
-	 * \brief An event handler function, called when the page selection is changed. 
-	 * \param wxNotebookEvent&  
-	*/
-
-  virtual void OnSelChange(wxNotebookEvent&  event ) ;
-	/**
-	 * \brief Deletes the specified page, without deleting the associated window. 
-	 * \param int  
-	*/
-
-  bool RemovePage(int  page ) ;
-	/**
-	 * \brief Sets the image list for the page control. It does not take
-ownership of the image list, you must delete it yourself. 
-	 * \param wxImageList*  
-	*/
-
-  void SetImageList(wxImageList*  imageList ) ;
-	/**
-	 * \brief Sets the amount of space around each page's icon and label, in pixels.
-
-  The vertical padding cannot be changed in wxGTK. 
-	 * \param const wxSize&  
-	*/
-
-  void SetPadding(const wxSize&  padding ) ;
-	/**
-	 * \brief Sets the width and height of the pages.
-
-  This method is currently not implemented for wxGTK. 
-	 * \param const wxSize&  
-	*/
-
-  void SetPageSize(const wxSize&  size ) ;
-	/**
-	 * \brief Sets the image index for the given page.   is an index into
-the image list which was set with  . 
-	 * \param int  
-	 * \param int   
-	*/
-
-  bool SetPageImage(int  page , int  image ) ;
-	/**
-	 * \brief Sets the text for the given page. 
-	 * \param int  
-	 * \param const wxString&   
-	*/
-
-  bool SetPageText(int  page , const wxString&  text ) ;
-	/**
-	 * \brief Sets the selection for the given page, returning the previous selection. 
-	 * \param int  
-	*/
-
-  int SetSelection(int  page ) ;
+  bool InsertPage(int index, 
+				  wxNotebookPage* page, 
+				  const wxString& text, 
+				  bool select = false, 
+				  int imageId = -1);
+  virtual void OnSelChange(wxNotebookEvent& event);
+  bool RemovePage(int page);
+  void SetImageList(wxImageList* imageList);
+  void SetPadding(const wxSize& padding);
+  void SetPageSize(const wxSize& size);
+  bool SetPageImage(int page, int image);
+  bool SetPageText(int page, const wxString& text);
+  int SetSelection(int page);
 };
 
 
