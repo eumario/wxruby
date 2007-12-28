e3414c015c500dd8c465ac9658d00581398c9e06
Alex Fenton <alex@pressure.to>
2007-12-28 12:12:07 +0000

    Actually commit real changes to lib/treectrl.rb that should have been in SVN:1473

--- lib/wx/classes/treectrl.rb
+++ lib/wx/classes/treectrl.rb
@@ -15,4 +15,30 @@ class Wx::TreeCtrl
     end
     kids
   end
+
+  # Returns a Wx::Rect corresponding to the edges of an individual tree
+  # item, including the button, identified by id. The standard wxWidgets
+  # API for getting the pixel location of an item is unrubyish, using an
+  # input/output parameter. But since the underlying get_bounding_rect
+  # method works, it's easier to fix the API in Ruby than adding more to
+  # the already-toxic swig interface TreeCtrl.i file.
+  def get_item_rect(tree_item_id)
+    rect = Wx::Rect.new
+    if get_bounding_rect(tree_item_id, rect, false)
+      return rect
+    else
+      return nil
+    end
+  end
+
+  # Returns a Wx::Rect corresponding to the edges of an individual tree
+  # item's text label. See above.
+  def get_label_rect(tree_item_id)
+    rect = Wx::Rect.new
+    if get_bounding_rect(tree_item_id, rect, true)
+      return rect
+    else
+      nil
+    end
+  end
 end
