91b03ed0034f1cdb5a7e03aaae61071f75e5401f
Alex Fenton <alex@pressure.to>
2008-01-04 21:12:44 +0000

    Allow Linux build to be shared/dynamic instead of static

--- wxruby2/rake/rakelinux.rb
+++ wxruby2/rake/rakelinux.rb
@@ -16,19 +16,17 @@ $extra_ldflags = '-shared'
 # This class is not available on WXGTK
 $excluded_classes << 'PrinterDC'
 
-# This appears to work as of 2006-08-23, Ubuntu 6.06, wx 2.6.3
-# You must have a non-shared, unicode wx configuration available
-$link_wx_statically = true
-if($link_wx_statically)
-	$extra_libs = "-Wl,-Bdynamic -lgtk-x11-2.0 -lgdk-x11-2.0 -latk-1.0 " + 
-	"-lgdk_pixbuf-2.0 -lpangoxft-1.0 -lpangox-1.0 -lpango-1.0 " + 
-	"-lgobject-2.0 -lgmodule-2.0 -lgthread-2.0 -lglib-2.0 "
-	libs = $wx_libs.split(' ')
-	libs.collect! do | lib |
-		if(lib =~ /lwx_/)
-			lib = "-Wl,-Bstatic #{lib} -Wl,-Bdynamic "
-		end
-		lib
-	end
-	$wx_libs = libs.join(' ')
+# Extra libraries that are required on Linux
+$extra_libs = "-Wl,-Bdynamic -lgtk-x11-2.0 -lgdk-x11-2.0 -latk-1.0 " + 
+  "-lgdk_pixbuf-2.0 -lpangoxft-1.0 -lpangox-1.0 -lpango-1.0 " + 
+  "-lgobject-2.0 -lgmodule-2.0 -lgthread-2.0 -lglib-2.0 "
+libs = $wx_libs.split(' ')
+libs.collect! do | lib |
+  if $static_build and lib =~ /lwx_/
+    lib = "-Wl,-Bstatic #{lib} -Wl,-Bdynamic "
+  end
+  lib
 end
+
+$wx_libs = libs.join(' ')
+
