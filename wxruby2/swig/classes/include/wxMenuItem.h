fbb77c4421e905c33733ad8dd6a84c33d2493bca
Alex Fenton <alex@pressure.to>
2008-02-05 17:50:47 +0000

    Tidy up and correct wxMenuItem.h for 2.8, adding get_label_text etc

--- wxruby2/swig/classes/include/wxMenuItem.h
+++ wxruby2/swig/classes/include/wxMenuItem.h
@@ -1,195 +1,79 @@
-// wxMenuItem.h
-// This file was automatically generated
-// by extractxml.rb, part of the wxRuby project
-// Do not make changes directly to this file!
+// Copyright 2004-2008, wxRuby development team
+// released under the MIT-like wxRuby2 license
 
 #if !defined(_wxMenuItem_h_)
 #define _wxMenuItem_h_
 class wxMenuItem : public wxObject
 {
 public:
-	/**
-	 * \brief Constructs a wxMenuItem object. 
-	 * \param wxMenu*  
-	 * \param int  
-	 * \param const wxString&   
-	 * \param const wxString&   
-	 * \param wxItemKind   
-	 * \param wxMenu*  
-	*/
-
   wxMenuItem(wxMenu *parentMenu = (wxMenu *)NULL,
              int id = wxID_SEPARATOR,
              const wxString& name = wxEmptyString,
              const wxString& help = wxEmptyString,
              wxItemKind kind = wxITEM_NORMAL,
              wxMenu *subMenu = (wxMenu *)NULL);
-	/**
-	 * \brief Destructor. 
-	*/
-
-#if 0 // DEPRECATED
-  // compatibility only, don't use in new code
-  wxMenuItem(wxMenu *parentMenu,
-             int id,
-             const wxString& text,
-             const wxString& help,
-             bool isCheckable,
-             wxMenu *subMenu = (wxMenu *)NULL);
-#endif
-
   virtual  ~wxMenuItem() ;
-	/**
-	 * \brief Checks or unchecks the menu item. 
-	 * \param bool  
-	*/
 
   void Check(bool bDoCheck = true);
-	/**
-	 * \brief Deletes the submenu, if any. 
-	*/
+  void Enable(bool bDoEnable = true);
 
-  // mark item as belonging to the given radio group
+  wxString GetHelp() const;
+  int GetId() const;
+  wxString GetItemLabel() const;
+  wxString GetItemLabelText() const;
+  wxItemKind GetKind() const;
+  wxString GetLabel() const; // deprecated
+  static wxString GetLabelText(const wxString& text) const;
+  wxString GetText() const; // deprecated
+
+  wxMenu *GetMenu() const;
+  wxMenu* GetSubMenu() const;
+
+  bool IsCheckable() const;
+  bool IsChecked() const;
+  bool IsEnabled() const;
+  bool IsSeparator() const;
+  bool IsSubMenu() const;
+
   void SetAsRadioGroupStart();
+  void SetHelp(const wxString& str);
+  void SetId(int itemid);
+  void SetItemLabel(const wxString& label);
+  void SetKind(wxItemKind kind);
+  void SetMenu(wxMenu* menu);
   void SetRadioGroupStart(int start);
   void SetRadioGroupEnd(int end);
-
-  void Enable(bool bDoEnable = true);
+  void SetSubMenu(wxMenu *menu);
+  void SetText(const wxString& strName);
 
   void Toggle();
 
-  wxString GetHelp();
-	/**
-	 * \brief Returns the menu item identifier. 
-	*/
 
-  int GetRealId();
-  
-  void SetId(int itemid);
-  int GetId();
-	/**
-	 * \brief Returns the item kind, one of  ,  , 
-  or  . 
-	*/
-
-  void SetKind(wxItemKind kind);
-  wxItemKind GetKind();
-	/**
-	 * \brief  
-	*/
-
-  wxString GetLabel();
-	/**
-	 * \brief  
-	 * \param const wxString&   
-	*/
-
-  static wxString GetLabelFromText(const wxString&  text ) ;
-	/**
-	 * \brief Gets the width of the menu item checkmark bitmap (Windows only). 
-	*/
-
-	/**
-	 * \brief  
-	*/
-
-  wxString GetText();
-	/**
-	 * \brief Returns the submenu associated with the menu item, or NULL if there isn't one. 
-	*/
-
-  bool IsSubMenu();
-  
-  void SetSubMenu(wxMenu *menu);
-  
-  wxMenu* GetSubMenu();
-	/**
-	 * \brief Returns the text colour associated with the menu item (Windows only). 
-	*/
-
-  virtual void SetCheckable(bool checkable);
-
-  bool IsCheckable();
-	/**
-	 * \brief Returns true if the item is checked. 
-	*/
-
-  bool IsChecked();
-	/**
-	 * \brief Returns true if the item is enabled. 
-	*/
-
-  bool IsEnabled();
-	/**
-	 * \brief Returns true if the item is a separator. 
-	*/
-
-  bool IsSeparator();
-
+// Various methods available only on Windows
 #if defined(__WXMSW__)
-	/**
-	 * \brief Sets the background colour associated with the menu item (Windows only). 
-	 * \param const wxColour&   
-	*/
-  void SetBackgroundColour(const wxColour& colour);
+  wxColour& GetBackgroundColour() const;
+  wxBitmap& GetBitmap(bool checked = true) const;
+  wxFont& GetFont() const;
+  int GetMarginWidth();
+  wxColour& GetTextColour(const wxColour& colour);
+
+  void SetBackgroundColour(const wxColour& colour) const;
+  void SetBitmaps(const wxBitmap& checked, 
+                  const wxBitmap& unchecked = wxNullBitmap);
+  void SetFont(const wxFont& font);
+  void SetMarginWidth(int width);
+  void SetTextColour(const wxColour& colour);
 #endif
 
 #if defined(__WXMSW__) || defined (__WXGTK__)
   void SetBitmap(const wxBitmap& bmp);
 #endif
 
-#if defined(__WXMSW__)
-  void SetBitmaps(const wxBitmap& checked, const wxBitmap& unchecked = wxNullBitmap);
-
-  void SetFont(const wxFont& font);
-#endif
-
-  void SetHelp(const wxString& str);
-
-#if defined(__WXMSW__)
-	/**
-	 * \brief Sets the width of the menu item checkmark bitmap (Windows only). 
-	 * \param int  
-	*/
-  void SetMarginWidth(int width);
-#endif
-
-	/**
-	 * \brief Sets the text associated with the menu item. 
-	 * \param const wxString&   
-	*/
-
-  void SetText(const wxString& strName);
-
-#if defined(__WXMSW__)
-	/**
-	 * \brief Sets the text colour associated with the menu item (Windows only). 
-	 * \param const wxColour&   
-	*/
-  void SetTextColour(const wxColour& colour);
-#endif
-
-  wxMenu *GetMenu();
-  void SetMenu(wxMenu* menu);
-  
 #if wxUSE_ACCEL
-  // extract the accelerator from the given menu string, return NULL if none
-  // found
   static wxAcceleratorEntry *GetAccelFromString(const wxString& label);
-
-  // get our accelerator or NULL (caller must delete the pointer)
   virtual wxAcceleratorEntry *GetAccel();
-
-  // set the accel for this item - this may also be done indirectly with
-  // SetText()
   virtual void SetAccel(wxAcceleratorEntry *accel);
 #endif // wxUSE_ACCEL  
-
-
-  // Depreciated
-  //void SetName(const wxString& str);
-  //const wxString& GetName();
-
 };
 
 
