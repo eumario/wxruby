597cc9990f1dda035ea2f406c5eb01f65714702b
Alex Fenton <alex@pressure.to>
2008-02-06 23:57:51 +0000

    Simplify images demo

--- samples/drawing/images.rb
+++ samples/drawing/images.rb
@@ -17,32 +17,26 @@ end
 # window. This one uses a small PNG file, but other formats such as JPEG
 # are supported - see documentation for more details.
 
-class MyFrame < Wx::Frame
-  def initialize(title)
-    super(nil, -1, title)
-    evt_paint { on_paint }
-    
+class ImageFrame < Wx::Frame
+  def initialize
+    super(nil, :title => 'Simple image demo')
+
+    # Load a PNG bitmap from a file for drawing
     img_file = File.join( File.dirname(__FILE__), 'paperclip.png')
-    # first load the image into an Image object
-    image = Wx::Image.new(img_file)
-    # then create a Bitmap suitable for drawing
-    @bitmap = Wx::Bitmap.new(image)
+    @bitmap = Wx::Bitmap.new(img_file, Wx::BITMAP_TYPE_PNG)
+
+    # Set up the drawing to be done when the frame needs re-painting
+    evt_paint :on_paint
   end
 
   def on_paint
     paint do | dc |
-      dc.clear
       dc.draw_bitmap(@bitmap, 0, 0, false)
     end
   end
 end
 
-class ImagesApp < Wx::App
-  def on_init
-    frame = MyFrame.new("Simple Image Demo")
-	frame.show
-  end
+Wx::App.run do
+  ImageFrame.new.show
 end
 
-a = ImagesApp.new
-a.main_loop()
