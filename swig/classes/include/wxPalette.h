d29884819cdd8cb70ada70ffe19fc429e7b86be4
Alex Fenton <alex@pressure.to>
2008-02-07 15:47:47 +0000

    Tidy and correct Palette header, but wrapping is problematic

--- swig/classes/include/wxPalette.h
+++ swig/classes/include/wxPalette.h
@@ -1,71 +1,30 @@
-// wxPalette.h
-// This file was automatically generated
-// by extractxml.rb, part of the wxRuby project
-// Do not make changes directly to this file!
+// Copyright 2004-2008, wxRuby development team
+// released under the MIT-like wxRuby2 license
 
 #if !defined(_wxPalette_h_)
 #define _wxPalette_h_
 class wxPalette : public wxGDIObject
 {
 public:
-	/**
-	 * \brief Default constructor. 
-	*/
-
    wxPalette() ;
-	/**
-	 * \brief Copy constructor. This uses reference counting so is a cheap operation. 
-	 * \param const wxPalette&  
-	*/
-
-   wxPalette(const wxPalette&  palette ) ;
-	/**
-	 * \brief Creates a palette from arrays of size  , one for each
-red, blue or green component. 
-	 * \param int  
-	 * \param const unsigned char*   
-	 * \param const unsigned char*   
-	 * \param const unsigned char*   
-	*/
-
-   wxPalette(int  n , const unsigned char*  red , const unsigned char*  green , const unsigned char*  blue ) ;
-	/**
-	 * \brief Destructor. 
-	*/
-
+   wxPalette(const wxPalette&  palette );
+   wxPalette(int n, 
+             const unsigned char* red, 
+             const unsigned char* green, 
+             const unsigned char* blue );
   virtual  ~wxPalette() ;
-	/**
-	 * \brief Creates a palette from arrays of size  , one for each
-red, blue or green component. 
-	 * \param int  
-	 * \param const unsigned char*   
-	 * \param const unsigned char*   
-	 * \param const unsigned char*   
-	*/
-
-  bool Create(int  n , const unsigned char*  red , const unsigned char*  green , const unsigned char*  blue ) ;
-	/**
-	 * \brief Returns a pixel value (index into the palette) for the given RGB values. 
-	 * \param const unsigned char   
-	 * \param const unsigned char   
-	 * \param const unsigned char   
-	*/
-
-  int GetPixel(const unsigned char  red , const unsigned char  green , const unsigned char  blue ) const;
-	/**
-	 * \brief Returns RGB values for a given palette index. 
-	 * \param int  
-	 * \param const unsigned char*   
-	 * \param const unsigned char*   
-	 * \param const unsigned char*   
-	*/
-
-  bool GetRGB(int  pixel , const unsigned char*  red , const unsigned char*  green , const unsigned char*  blue ) const;
-	/**
-	 * \brief Returns true if palette data is present. 
-	*/
-
-  bool Ok() const;
+  bool Create(int n, 
+              const unsigned char* red, 
+              const unsigned char* green, 
+              const unsigned char* blue);
+  int GetPixel(unsigned char red, 
+               unsigned char green, 
+               unsigned char blue) const;
+  bool GetRGB(int pixel, 
+              unsigned char* red, 
+              unsigned char* green, 
+              unsigned char* blue) const;
+  bool IsOk() const;
 };
 
 
