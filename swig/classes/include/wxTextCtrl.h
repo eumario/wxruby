f29ffcc9b198ce11b1e3925f7b16653735a106d8
Alex Fenton <alex@pressure.to>
2008-02-07 01:19:22 +0000

    Remove "virtual" tag from methods that shouldn't have it, fixing BUG:8068

--- swig/classes/include/wxTextCtrl.h
+++ swig/classes/include/wxTextCtrl.h
@@ -25,33 +25,33 @@ public:
                 const wxValidator& validator = wxDefaultValidator,
                 const wxString &name = wxTextCtrlNameStr);
 
-    virtual wxString GetValue() const;
+    wxString GetValue() const;
     virtual void SetValue(const wxString& value); // deprecated since 2.8
 	virtual void ChangeValue(const wxString& value);
 
 
-    virtual int GetLineLength(long lineNo) const;
-    virtual wxString GetLineText(long lineNo) const;
-    virtual int GetNumberOfLines() const;
-    virtual bool IsModified() const;
-    virtual bool IsEditable() const;
+    int GetLineLength(long lineNo) const;
+    wxString GetLineText(long lineNo) const;
+    int GetNumberOfLines() const;
+    bool IsModified() const;
+    bool IsEditable() const;
 	bool IsEmpty() const;
 
     virtual void GetSelection(long* from, long* to) const;
     virtual void Clear();
     virtual void Replace(long from, long to, const wxString& value);
     virtual void Remove(long from, long to);
-    virtual void MarkDirty();
-    virtual void DiscardEdits();
+    void MarkDirty();
+    void DiscardEdits();
     virtual void SetMaxLength(unsigned long len);
-    virtual void WriteText(const wxString& text);
-    virtual void AppendText(const wxString& text);
-    virtual bool SetStyle(long start, long end, const wxTextAttr& style);
-    virtual long XYToPosition(long x, long y) const;
-    virtual bool PositionToXY(long pos, long *x, long *y) const;
-    virtual void ShowPosition(long pos);
+    void WriteText(const wxString& text);
+    void AppendText(const wxString& text);
+    bool SetStyle(long start, long end, const wxTextAttr& style);
+    long XYToPosition(long x, long y) const;
+    bool PositionToXY(long pos, long *x, long *y) const;
+    void ShowPosition(long pos);
 
-    virtual wxTextCtrlHitTestResult HitTest(const wxPoint& pt,
+    wxTextCtrlHitTestResult HitTest(const wxPoint& pt,
                                             wxTextCoord *col,
                                             wxTextCoord *row) const;
     virtual void Copy();
@@ -59,15 +59,20 @@ public:
     virtual void Paste();
     virtual void Undo();
     virtual void Redo();
+
+    virtual bool CanCopy() const;
+    virtual bool CanCut() const;
+    virtual bool CanPaste() const;
     virtual bool CanUndo() const;
     virtual bool CanRedo() const;
+
     virtual void SetInsertionPoint(long pos);
     virtual void SetInsertionPointEnd();
     virtual long GetInsertionPoint() const;
     virtual wxTextPos GetLastPosition() const;
     virtual void SetSelection(long from, long to);
     virtual void SetEditable(bool editable);
-    virtual bool Enable( bool enable = true );
+
     void OnDropFiles( wxDropFilesEvent &event );
     void OnChar( wxKeyEvent &event );
     void OnCut(wxCommandEvent& event);
@@ -86,29 +91,6 @@ public:
 
 
     void SetModified(bool modified);
-
-	// Although the following were public in the .h file,
-	// I believe they are not part of the public API
-	//GtkWidget* GetConnectWidget();
-    //bool IsOwnGtkWindow( GdkWindow *window );
-    //void DoApplyWidgetStyle(GtkRcStyle *style);
-    //void CalculateScrollbar();
-    //void OnInternalIdle();
-    //void SetUpdateFont(bool ) { }
-
-    //virtual void Freeze();
-    //virtual void Thaw();
-    //virtual bool ScrollLines(int lines);
-    //virtual bool ScrollPages(int pages);
-    //virtual void OnParentEnable( bool enable ) ;
-    //void IgnoreNextTextUpdate();
-    //bool IgnoreTextUpdate();
-    //static wxVisualAttributes
-    //GetClassDefaultAttributes(wxWindowVariant variant = wxWINDOW_VARIANT_NORMAL);
-	
-	// From wxTextCtrlBase
-    //virtual wxString GetValue() const = 0;
-    //virtual void SetValue(const wxString& value) = 0;
 
     virtual wxString GetRange(long from, long to) const;
 
@@ -130,43 +112,13 @@ public:
     virtual bool LoadFile(const wxString& file);
     virtual bool SaveFile(const wxString& file = wxEmptyString);
 
-    //virtual void MarkDirty() = 0;
-    //virtual void DiscardEdits() = 0;
-
-    //virtual void WriteText(const wxString& text) = 0;
-    //virtual void AppendText(const wxString& text) = 0;
-
-    virtual bool EmulateKeyPress(const wxKeyEvent& event);
-    virtual bool GetStyle(long position, wxTextAttr& style);
-    virtual bool SetDefaultStyle(const wxTextAttr& style);
-    virtual const wxTextAttr& GetDefaultStyle() const;
-
-    //virtual long XYToPosition(long x, long y) const = 0;
-    //virtual bool PositionToXY(long pos, long *x, long *y) const = 0;
-    //virtual void ShowPosition(long pos) = 0;
-
-    //virtual void Copy() = 0;
-    //virtual void Cut() = 0;
-    //virtual void Paste() = 0;
-
-    virtual bool CanCopy() const;
-    virtual bool CanCut() const;
-    virtual bool CanPaste() const;
-
-
-    //virtual void Undo() = 0;
-    //virtual void Redo() = 0;
-    //virtual bool CanUndo() const = 0;
-    //virtual bool CanRedo() const = 0;
-    //virtual void SetInsertionPoint(long pos) = 0;
-    //virtual void SetInsertionPointEnd() = 0;
-    //virtual long GetInsertionPoint() const = 0;
-    //virtual wxTextPos GetLastPosition() const = 0;
-    //virtual void SetSelection(long from, long to) = 0;
+    bool EmulateKeyPress(const wxKeyEvent& event);
+    bool GetStyle(long position, wxTextAttr& style);
+    bool SetDefaultStyle(const wxTextAttr& style);
+    const wxTextAttr& GetDefaultStyle() const;
     
 	virtual void SelectAll();
     
-	//virtual void SetEditable(bool editable) = 0;
 
    // int overflow(int i);
     wxTextCtrl& operator<<(const wxString& s);
