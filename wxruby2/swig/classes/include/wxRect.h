8df5f3f78ab9631bcc87e2e5c601c1d8eaf5f6a3
Alex Fenton <alex@pressure.to>
2008-02-09 12:09:10 +0000

    Add some 2.8 convenience methods to Rect: Contains, CentreIn

--- wxruby2/swig/classes/include/wxRect.h
+++ wxruby2/swig/classes/include/wxRect.h
@@ -1,188 +1,73 @@
-// wxRect.h
-// This file was automatically generated
-// by extractxml.rb, part of the wxRuby project
-// Do not make changes directly to this file!
+// Copyright 2004-2008, wxRuby development team
+// released under the MIT-like wxRuby2 license
 
 #if !defined(_wxRect_h_)
 #define _wxRect_h_
 class wxRect
 {
 public:
-	/**
-	 * \brief Default constructor. 
-	*/
+   wxRect();
+   wxRect(int  x , int  y , int  width , int  height);
+   wxRect(const wxPoint& topLeft, const wxPoint& bottomRight);
+   wxRect(const wxPoint& pos, const wxSize&  size);
 
-   wxRect() ;
-	/**
-	 * \brief Creates a wxRect object from x, y, width and height values. 
-	 * \param int  
-	 * \param int  
-	 * \param int  
-	 * \param int  
-	*/
-
-   wxRect(int  x , int  y , int  width , int  height ) ;
-	/**
-	 * \brief Creates a wxRect object from top-left and bottom-right points. 
-	 * \param const wxPoint&  
-	 * \param const wxPoint&  
-	*/
-
-   wxRect(const wxPoint&  topLeft , const wxPoint&  bottomRight ) ;
-	/**
-	 * \brief Creates a wxRect object from position and size values. 
-	 * \param const wxPoint&  
-	 * \param const wxSize&  
-	*/
-
-   wxRect(const wxPoint&  pos , const wxSize&  size ) ;
-	/**
-	 * \brief  
-	 * \param wxCoord   
-	 * \param wxCoord   
-	*/
-
-  void Deflate(wxCoord  dx , wxCoord  dy ) ;
-	/**
-	 * \brief  
-	 * \param wxCoord   
-	*/
-
-  void Deflate(wxCoord  diff ) ;
-	/**
-	 * \brief Decrease the rectangle size by   in x direction and   in y
-direction. Both (or one of) parameters may be negative to increase the
-rectngle size. This method is the opposite of  .
-
-The second form uses the same   for both   and  .
-
-The first two versions modify the rectangle in place, the last one returns a
-new rectangle leaving this one unchanged. 
-	 * \param wxCoord   
-	 * \param wxCoord   
-	*/
-
-  wxRect Deflate(wxCoord  dx , wxCoord  dy ) const;
-	/**
-	 * \brief Gets the bottom point of the rectangle. 
-	*/
-
-  int GetBottom() const;
-	/**
-	 * \brief Gets the height member. 
-	*/
-
-  int GetHeight() const;
-	/**
-	 * \brief Gets the left point of the rectangle (the same as  ). 
-	*/
-
-  int GetLeft() const;
-	/**
-	 * \brief Gets the position. 
-	*/
-
-  wxPoint GetPosition() const;
-	/**
-	 * \brief Gets the right point of the rectangle. 
-	*/
-
-  int GetRight() const;
-	/**
-	 * \brief Gets the size. 
-	*/
-
-  wxSize GetSize() const;
-	/**
-	 * \brief Gets the top point of the rectangle (the same as  ). 
-	*/
-
-  int GetTop() const;
-	/**
-	 * \brief Gets the width member. 
-	*/
-
-  int GetWidth() const;
-	/**
-	 * \brief Gets the x member. 
-	*/
-
-  int GetX() const;
-	/**
-	 * \brief Gets the y member. 
-	*/
-
-  int GetY() const;
-	/**
-	 * \brief  
-	 * \param wxCoord   
-	 * \param wxCoord   
-	*/
-
-  void Inflate(wxCoord  dx , wxCoord  dy ) ;
-	/**
-	 * \brief  
-	 * \param wxCoord   
-	*/
-
-  void Inflate(wxCoord  diff ) ;
-	/**
-	 * \brief Increase the rectangle size by   in x direction and   in y
-direction. Both (or one of) parameters may be negative to decrease the
-rectangle size.
-
-The second form uses the same   for both   and  .
-
-The first two versions modify the rectangle in place, the last one returns a
-new rectangle leaving this one unchanged. 
-	 * \param wxCoord   
-	 * \param wxCoord   
-	*/
-
-  wxRect Inflate(wxCoord  dx , wxCoord  dy ) const;
-	/**
-	 * \brief  
-	 * \param wxCoord   
-	 * \param wxCoord   
-	*/
-
-  void Offset(wxCoord  dx , wxCoord  dy ) ;
-	/**
-	 * \brief Moves the rectangle by the specified offset. If   is positive, the
-rectangle is moved to the right, if   is positive, it is moved to the
-bottom, otherwise it is moved to the left or top respectively. 
-	 * \param const wxPoint&   
-	*/
-
-  void Offset(const wxPoint&  pt ) ;
-	/**
-	 * \brief Sets the height. 
-	 * \param int  
-	*/
-
-  void SetHeight(int  height ) ;
-	/**
-	 * \brief Sets the width. 
-	 * \param int  
-	*/
-
-  void SetWidth(int  width ) ;
-	/**
-	 * \brief Sets the x position. 
-	 * \param int  
-	*/
-
-  void SetX(int  x ) ;
-	/**
-	 * \brief Sets the y position. 
-	 * \param int  
-	*/
-
-  void SetY(int  y ) ;
-
-  // Missing from XML
   int x, y;
   int width, height;
+
+  // centre this rectangle in the given (usually, but not necessarily,
+  // larger) one
+  wxRect CentreIn(const wxRect& r, int dir = wxBOTH) const;
+  wxRect CenterIn(const wxRect& r, int dir = wxBOTH) const;
+
+  // return true if the point is (not strcitly) inside the rect
+  bool Contains(int x, int y) const;
+  bool Contains(const wxPoint& pt) const;
+  // return true if the rectangle is (not strcitly) inside the rect
+  bool Contains(const wxRect& rect) const;
+
+  void Deflate(wxCoord dx , wxCoord dy);
+  void Deflate(wxCoord diff ) ;
+  wxRect Deflate(wxCoord dx, wxCoord dy) const;
+
+  int GetHeight() const;
+  int GetWidth() const;
+
+  int GetBottom() const;
+  int GetLeft() const;
+  int GetRight() const;
+  int GetTop() const;
+
+  wxSize GetSize() const;
+  wxPoint GetPosition() const;
+
+  wxPoint GetTopLeft() const;
+  wxPoint GetTopRight() const;
+  wxPoint GetBottomLeft() const;
+  wxPoint GetBottomRight() const;
+
+  int GetX() const;
+  int GetY() const;
+
+  void Inflate(wxCoord dx, wxCoord dy);
+  void Inflate(wxCoord diff);
+  void Inflate(const wxSize diff);
+  wxRect Inflate(wxCoord dx, wxCoord dy) const;
+
+  wxRect Intersect(const wxRect& rect) const;
+
+  bool IsEmpty() const;
+
+  void Offset(wxCoord dx, wxCoord dy);
+  void Offset(const wxPoint& pt);
+
+  void SetHeight(int height);
+  void SetWidth(int width);
+  void SetX(int  x);
+  void SetY(int  y);
+
+  void SetSize(const wxSize &s);
+
+  wxRect Union(const wxRect& rect) const;
 };
 
 
