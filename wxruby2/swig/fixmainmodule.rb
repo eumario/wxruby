a6bb570b5666338adc061b6cd9ab6445c4b38269
Alex Fenton <alex@pressure.to>
2008-02-28 15:05:32 +0000

    Upgrade to support latest SWIG; 1.3.31 is no longer supported

--- wxruby2/swig/fixmainmodule.rb
+++ wxruby2/swig/fixmainmodule.rb
@@ -34,28 +34,6 @@ File.open(ARGV[0], "w") do | out |
 			found_main_module = true
         end
 
-        # Fix 1.3.29's ruby tracking
-        # TODO: Should be fixed in (or different with) 1.3.30 but isn't yet
-#        if(ENV['SWIGVER'] == 'SWIG Version 1.3.29')
-          if(line.index("static VALUE swig_ruby_trackings"))
-            line = "VALUE swig_ruby_trackings = 0;"
-          end
-
-          if(line.index("static ID swig_ruby_hash_delete"))
-            line = "ID swig_ruby_hash_delete = 0;"
-          end
-
-          if(line.index("swig_ruby_trackings = rb_hash_new();"))
-            line = "  if(swig_ruby_trackings == 0) {\n" +
-                   line;
-          end
-			
-          if(line.index("swig_ruby_hash_delete = rb_intern(\"delete\");"))
-            line = line + "\n" +
-                   "rb_global_variable(&swig_ruby_trackings);\n" +
-                   "  }";
-          end
-
           if(line.index("char* type_name = RSTRING(value)->ptr;"))
             line = "        const char* type_name = (value == Qnil) ? \"\" : RSTRING(value)->ptr;\n";
           end
