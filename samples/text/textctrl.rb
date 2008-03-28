8c6596a314ee042504d2692e2f279477122691c6
Alex Fenton <alex@pressure.to>
2008-03-28 00:51:10 +0000

    Sugarify textctrl sample

--- samples/text/textctrl.rb
+++ samples/text/textctrl.rb
@@ -1,5 +1,5 @@
 #!/usr/bin/env ruby
-# wxRuby2 Sample Code. Copyright (c) 2004-2006 Kevin B. Smith
+# wxRuby2 Sample Code. Copyright (c) 2004-2008 wxRuby development team
 # Freely reusable code: see SAMPLES-LICENSE.TXT for details
 
 begin
@@ -19,8 +19,8 @@ class InformativeTextCtrl < Wx::TextCtrl
   # These text controls are multiline, and may have rich (coloured,
   # styled) text in them
   STYLE = Wx::TE_MULTILINE|Wx::TE_RICH|Wx::TE_RICH2
-  def initialize(parent, text)
-    super(parent, -1, text, Wx::DEFAULT_POSITION, Wx::DEFAULT_SIZE, STYLE)
+  def initialize(parent, text = '')
+    super(parent, :value => text, :style => STYLE)
   end
 
   # more ruby-ish
@@ -45,29 +45,28 @@ end
 class LogTextCtrl < Wx::TextCtrl
   STYLE = Wx::TE_READONLY|Wx::TE_MULTILINE
   def initialize(parent)
-    super(parent, -1, '', Wx::DEFAULT_POSITION, Wx::DEFAULT_SIZE, STYLE)
+    super(parent, :style => STYLE)
   end
 end
 
 class TextCtrlFrame < Wx::Frame
-  def initialize(title, pos, size)
-    super(nil, -1, title, pos, size)
+  def initialize(*args)
+    super(nil, *args)
 
-    panel = Wx::Panel.new(self, -1)
+    panel = Wx::Panel.new(self)
     sizer = Wx::BoxSizer.new(Wx::VERTICAL)
 
-    @textctrl = InformativeTextCtrl.new(panel, '')
+    @textctrl = InformativeTextCtrl.new(panel)
     populate_textctrl()
     sizer.add(@textctrl, 2, Wx::GROW|Wx::ALL, 2)
 
-    button = Wx::Button.new(panel, -1, 'Get Info')
+    button = Wx::Button.new(panel, :label => 'Get Info')
     sizer.add(button, 0, Wx::ADJUST_MINSIZE|Wx::ALL, 2 )
-    evt_button(button.get_id) { | e | on_click(e) }
+    evt_button button, :on_click
 
     @log = LogTextCtrl.new(panel)
     sizer.add(@log, 1, Wx::GROW|Wx::ALL, 2)
-
-    panel.set_sizer( sizer )
+    panel.sizer = sizer
   end
 
   def populate_textctrl()
@@ -86,39 +85,35 @@ class TextCtrlFrame < Wx::Frame
     menu_file = Wx::Menu.new()
     menu_file.append(Wx::ID_EXIT, "E&xit\tAlt-X", "Quit this program")
     menu_bar.append(menu_file, "&File")
-    evt_menu(Wx::ID_EXIT) { on_quit() }
+    evt_menu Wx::ID_EXIT, :on_quit
 
     menu_help = Wx::Menu.new()
     menu_help.append(Wx::ID_ABOUT, "&About...\tF1", "Show about dialog")
-    evt_menu(Wx::ID_ABOUT) { on_about() }
+    evt_menu Wx::ID_ABOUT, :on_about
     menu_bar.append(menu_help, "&Help")
 
-    set_menu_bar(menu_bar)
+    self.menu_bar = menu_bar
   end
 
-  def on_click(e)
-    @log.set_value( @textctrl.report() )
+  def on_click
+    @log.value = @textctrl.report
   end
 
-  def on_quit()
-    close(TRUE)
+  def on_quit
+    close
   end
 
-  def on_about()
+  def on_about
     msg =  sprintf("This is the About dialog of the textctrl sample.\n" \
                     "Welcome to %s", Wx::VERSION_STRING)
     message_box(msg, "About Minimal", Wx::OK|Wx::ICON_INFORMATION, self)
   end
 end
 
-class RbApp < Wx::App
-  def on_init()
-    frame = TextCtrlFrame.new("TextCtrl demonstration",
-                              Wx::Point.new(50, 50),
-                              Wx::Size.new(450, 340) )
-
-    frame.show(true)
-  end
+Wx::App.run do
+  frame = TextCtrlFrame.new( :title => "TextCtrl demonstration",
+                             :pos => [ 50, 50 ],
+                             :size => [ 450, 340 ] )
+  frame.show
 end
 
-RbApp.new().main_loop()
