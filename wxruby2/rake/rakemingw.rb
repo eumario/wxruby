e956cfc160bfc281006ac42db70b4c0a97cd091d
Alex Fenton <alex@pressure.to>
2008-04-01 14:29:23 +0000

    Allow debug builds on mingw; cleanup, comment, constantify

--- wxruby2/rake/rakemingw.rb
+++ wxruby2/rake/rakemingw.rb
@@ -6,50 +6,65 @@
 # First, common Windows settings
 require 'rake/rakewindows'
 
-# 
+# The version by which the wxWidgets library files are identified
+WXVERSION = '2.8'
+
 $cpp = "g++"
 $ld  = "g++ -shared" # not ld directly
 
+# Where the directory containing setup.h with the wxWidgets compile
+# options can be found; name depends on whether unicode and whether
+# debug or release. For now, only support unicode.
+if $debug_build
+  setup_dir = "msw-unicode-debug-static-#{WXVERSION}"
+else
+  setup_dir = "msw-unicode-release-static-#{WXVERSION}"
+end
 # Some secondary directories in the wxWidgets layout
 $WXINCDIR      = File.join("#$WXDIR", "include")
-if File.exists?(File.join("#$WXDIR",'lib','wx','include',
-													'msw-unicode-release-static-2.8',
-													'wx','setup.h'))
-$WXLIBDIR      = File.join("#$WXDIR", "lib")
-$WXSETUPINCDIR = File.join("#$WXDIR", "lib", "wx", "include", 
-                            "msw-unicode-release-static-2.8")
-elsif File.exists?(File.join("#$WXDIR",'build','lib','wx','include',
-															'msw-unicode-release-static-2.8',
-															'wx','setup.h'))
-	$WXLIBDIR				= File.join("#$WXDIR","build","lib")
-	$WXSETUPINCDIR	= File.join("#$WXDIR","build","lib","wx","include",
-															"msw-unicode-release-static-2.8")
+
+# Test for Windows-style builds (configured and built in root directory
+# of unpacked wxWidgets distribution) ...
+if File.exists?(File.join($WXDIR,'lib','wx','include', 
+                           setup_dir, 'wx','setup.h'))
+  $WXLIBDIR      = File.join($WXDIR, "lib")
+  $WXSETUPINCDIR = File.join($WXDIR, "lib", "wx", "include", setup_dir)
+# ... or Linux-style builds in a build subdirectory
+elsif File.exists?(File.join($WXDIR,'build','lib','wx','include',
+                              setup_dir, 'wx','setup.h'))
+  $WXLIBDIR      = File.join($WXDIR,"build","lib")
+  $WXSETUPINCDIR = File.join($WXDIR,"build","lib","wx","include", setup_dir)
 end
 
-
+# Define the location of setup.h that we'll be using
 WXWIDGETS_SETUP_H  = File.join($WXSETUPINCDIR, 'wx', 'setup.h')
 
+# Flags to be passed to the compiler
 $wx_cppflags = "-I#{$WXINCDIR} -D__WXMSW__ -I#{$WXSETUPINCDIR}"
 
 if $unicode_build
   $wx_cppflags += " -D_UNICODE -DUNICODE"
 end
 
+if $debug_build
+  $wx_cppflags += " -D_DEBUG -D__WXDEBUG__ -DWXDEBUG=1 "
+end
+
+
 # Variants within wxWidgets directory layout are identified by these tags
-$WXVERSION = '2.8'
 $DEBUGPOSTFIX   = $debug_build ? 'd' : ''
 $UNICODEPOSTFIX = $unicode_build ? 'u' : ''
 $POSTFIX = $UNICODEPOSTFIX + $DEBUGPOSTFIX
 
 # wxWidgets libraries that should be linked into wxRuby
 # odbc and db_table not required by wxruby
-windows_libs = %W|wx_msw#{$POSTFIX}-#{$WXVERSION}
-                  wxexpat#{$DEBUGPOSTFIX}-#{$WXVERSION}
-                  wxjpeg#{$DEBUGPOSTFIX}-#{$WXVERSION}
-                  wxpng#{$DEBUGPOSTFIX}-#{$WXVERSION}
-                  wxtiff#{$DEBUGPOSTFIX}-#{$WXVERSION}
-                  wxzlib#{$DEBUGPOSTFIX}-#{$WXVERSION}
-                  wxregex#{$POSTFIX}-#{$WXVERSION}|
+windows_libs = %W|wx_msw#{$POSTFIX}-#{WXVERSION}
+                  wxexpat#{$DEBUGPOSTFIX}-#{WXVERSION}
+                  wxjpeg#{$DEBUGPOSTFIX}-#{WXVERSION}
+                  wxpng#{$DEBUGPOSTFIX}-#{WXVERSION}
+                  wxtiff#{$DEBUGPOSTFIX}-#{WXVERSION}
+                  wxzlib#{$DEBUGPOSTFIX}-#{WXVERSION}
+                  wxregex#{$POSTFIX}-#{WXVERSION}|
 
 windows_libs.map! { | lib | File.join($WXLIBDIR, "lib#{lib}.a") }
 
@@ -57,7 +72,7 @@ windows_libs.map! { | lib | File.join($WXLIBDIR, "lib#{lib}.a") }
 # Test for presence of StyledTextCtrl (scintilla) library; link it in if
 # present, skip that class if not 
 scintilla_lib = File.join( $WXLIBDIR, 
-                           "libwx_msw#{$POSTFIX}_stc-#{$WXVERSION}.a" )
+                           "libwx_msw#{$POSTFIX}_stc-#{WXVERSION}.a" )
 if File.exists?(scintilla_lib)
   windows_libs << scintilla_lib 
 else
@@ -66,7 +81,7 @@ end
 
 # Test for presence of OpenGL library; link it in if present, skip that
 # class if not
-gl_lib = File.join( $WXLIBDIR, "libwx_wxmsw#{$POSTFIX}_gl-#{$WXVERSION}.a" )
+gl_lib = File.join( $WXLIBDIR, "libwx_wxmsw#{$POSTFIX}_gl-#{WXVERSION}.a" )
 if File.exists?(gl_lib)
   windows_libs << gl_lib 
 else
