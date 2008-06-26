fd98223bfb5d70fb00b349f6c0d0dfa0974e9c58
Alex Fenton <alex@pressure.to>
2008-06-26 17:26:00 +0000

    Enable default Panel ctor for use with XRC; tidy up and add two missing methods

--- swig/classes/include/wxPanel.h
+++ swig/classes/include/wxPanel.h
@@ -1,58 +1,29 @@
-// wxPanel.h
-// This file was automatically generated
-// by extractxml.rb, part of the wxRuby project
-// Do not make changes directly to this file!
+// Copyright 2004-2008 , wxRuby development team
+// released under the MIT-like wxRuby2 license
 
 #if !defined(_wxPanel_h_)
 #define _wxPanel_h_
 class wxPanel : public wxWindow
 {
 public:
-	/**
-	 * \brief Default constructor. 
-	*/
-
-  //wxPanel();
-	/**
-	 * \brief Constructor. 
-	 * \param wxWindow*   
-	 * \param wxWindowID   
-	 * \param const wxPoint&   
-	 * \param const wxSize&   
-	 * \param long  
-	 * \param const wxString&   
-	*/
-
-   wxPanel(wxWindow*  parent , wxWindowID  id = -1, const wxPoint&  pos = wxDefaultPosition, const wxSize&  size = wxDefaultSize, long style = wxTAB_TRAVERSAL|wxNO_BORDER, const wxString&  name = wxT("panel")) ;
-	/**
-	 * \brief Destructor. Deletes any child windows before deleting the physical window. 
-	*/
-
+  wxPanel();
+  wxPanel(wxWindow* parent, 
+          wxWindowID id = -1, 
+          const wxPoint& pos = wxDefaultPosition, 
+          const wxSize& size = wxDefaultSize, 
+          long style = wxTAB_TRAVERSAL|wxNO_BORDER, 
+          const wxString& name = wxT("panel")) ;
   virtual  ~wxPanel() ;
-	/**
-	 * \brief Used for two-step panel construction. See  
-for details. 
-	 * \param wxWindow*   
-	 * \param wxWindowID   
-	 * \param const wxPoint&   
-	 * \param const wxSize&   
-	 * \param long  
-	 * \param const wxString&   
-	*/
-
-  bool Create(wxWindow*  parent , wxWindowID  id , const wxPoint&  pos = wxDefaultPosition, const wxSize&  size = wxDefaultSize, long style = wxTAB_TRAVERSAL, const wxString&  name = wxT("panel")) ;
-	/**
-	 * \brief Sends a  , which
-in turn transfers data to the dialog via validators. 
-	*/
-
-  void InitDialog() ;
-	/**
-	 * \brief The default handler for wxEVT_SYS_COLOUR_CHANGED. 
-	 * \param wxSysColourChangedEvent&   
-	*/
-
+  bool Create(wxWindow* parent, 
+              wxWindowID id, 
+              const wxPoint& pos = wxDefaultPosition, 
+              const wxSize& size = wxDefaultSize, 
+              long style = wxTAB_TRAVERSAL, 
+              const wxString& name = wxT("panel")) ;
+  void InitDialog();
   virtual void OnSysColourChanged(wxSysColourChangedEvent&  event ) ;
+  virtual void SetFocus();
+  virtual void SetFocusIgnoringChildren();
 };
 
 
