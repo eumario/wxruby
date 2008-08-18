040dd3bd06299279e6da7dc12f9711b4ac96f3c3
Alex Fenton <alex@pressure.to>
2008-08-18 00:30:09 +0000

    Correct deprecated ListBox#selected -> ListBox#is_selected to avoid compile warn; tidy header

--- wxruby2/swig/classes/include/wxListBox.h
+++ wxruby2/swig/classes/include/wxListBox.h
@@ -1,7 +1,5 @@
-// wxListBox.h
-// This file was automatically generated
-// by extractxml.rb, part of the wxRuby project
-// Do not make changes directly to this file!
+// Copyright 2004-2008, wxRuby development team
+// released under the MIT-like wxRuby2 license
 
 #if !defined(_wxListBox_h_)
 #define _wxListBox_h_
@@ -9,19 +7,34 @@ class wxListBox : public wxControlWithItems
 {
 public:
    wxListBox() ;
-  wxListBox(wxWindow*  parent , wxWindowID  id , const wxPoint& pos = wxDefaultPosition, const wxSize& size = wxDefaultSize, int  n = 0 , const wxString  choices[] = NULL, long style = 0, const wxValidator&  validator = wxDefaultValidator, const wxString&  name = wxT("listBox"));
+  wxListBox(wxWindow* parent, wxWindowID id, 
+            const wxPoint& pos = wxDefaultPosition, 
+            const wxSize& size = wxDefaultSize, 
+            int  n = 0, 
+            const wxString  choices[] = NULL, 
+            long style = 0, 
+            const wxValidator&  validator = wxDefaultValidator, 
+            const wxString&  name = wxT("listBox"));
   virtual  ~wxListBox() ;
-  bool Create(wxWindow*  parent , wxWindowID  id , const wxPoint& pos , const wxSize& size , int  n , const wxString  choices[] = NULL, long style = 0, const wxValidator&  validator = wxDefaultValidator, const wxString&  name = wxT("listBox"));
-  void Deselect(int  n ) ;
-  int GetSelections(wxArrayInt&  selections ) const;
+  bool Create(wxWindow* parent, 
+              wxWindowID  id, 
+              const wxPoint& pos, 
+              const wxSize& size, 
+              int  n, 
+              const wxString  choices[] = NULL, 
+              long style = 0, 
+              const wxValidator& validator = wxDefaultValidator, 
+              const wxString&  name = wxT("listBox"));
+  void Deselect(int  n);
+  int GetSelections(wxArrayInt& selections) const;
   int HitTest(const wxPoint& pt) const;
   void InsertItems(int nItems, const wxString *items, int pos);
   void InsertItems(const wxArrayString& items, int pos);
-  bool Selected(int  n ) const;
+  bool IsSelected(int  n ) const;
   void Set(int  n , const wxString*  choices , void **clientData = NULL) ;
   void Set(const wxArrayString&  choices , void **clientData = NULL) ;
-  void SetFirstItem(int  n ) ;
-  void SetFirstItem(const wxString&  string ) ;
+  void SetFirstItem(int  n );
+  void SetFirstItem(const wxString& string);
 
   // the following were missing from the xml
   void Select(int n);
