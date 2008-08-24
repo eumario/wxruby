1baae781b1b85301a7f5b1639cb139094eac2799
Alex Fenton <alex@pressure.to>
2008-08-24 20:24:07 +0000

    Added wrapping for ContextHelp wx classes

--- wxruby2/swig/classes/include/wxContextHelpButton.h
+++ wxruby2/swig/classes/include/wxContextHelpButton.h
@@ -1,28 +1,17 @@
-// wxContextHelpButton.h
-// This file was automatically generated
-// by extractxml.rb, part of the wxRuby project
-// Do not make changes directly to this file!
+// Copyright 2004-2008, wxRuby development team
+// released under the MIT-like wxRuby2 license
 
 #if !defined(_wxContextHelpButton_h_)
 #define _wxContextHelpButton_h_
 class wxContextHelpButton : public wxBitmapButton
 {
 public:
-	/**
-	 * \brief Default constructor. 
-	*/
-
-   wxContextHelpButton() ;
-	/**
-	 * \brief Constructor, creating and showing a context help button. 
-	 * \param wxWindow*   
-	 * \param wxWindowID   
-	 * \param const wxPoint&   
-	 * \param const wxSize&   
-	 * \param long   
-	*/
-
-   wxContextHelpButton(wxWindow*  parent , wxWindowID  id = wxID_CONTEXT_HELP, const wxPoint&  pos = wxDefaultPosition, const wxSize&  size = wxDefaultSize, long  style = wxBU_AUTODRAW) ;
+   wxContextHelpButton(wxWindow* parent, 
+                       wxWindowID id = wxID_CONTEXT_HELP, 
+                       const wxPoint& pos = wxDefaultPosition, 
+                       const wxSize& size = wxDefaultSize, 
+                       long style = wxBU_AUTODRAW) ;
+   void OnContextHelp(wxCommandEvent& event);
 };
 
 
