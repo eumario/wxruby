6657318bc01e20ff91b12d680cf898084046256f
Alex Fenton <alex@pressure.to>
2008-08-24 20:25:51 +0000

    Added HelpEvent and associated event plumbing

--- wxruby2/swig/classes/include/wxHelpEvent.h
+++ wxruby2/swig/classes/include/wxHelpEvent.h
@@ -1,34 +1,15 @@
-// wxHelpEvent.h
-// This file was automatically generated
-// by extractxml.rb, part of the wxRuby project
-// Do not make changes directly to this file!
+// Copyright 2004-2008, wxRuby development team
+// released under the MIT-like wxRuby2 license
 
 #if !defined(_wxHelpEvent_h_)
 #define _wxHelpEvent_h_
 class wxHelpEvent : public wxCommandEvent
 {
 public:
-	/**
-	 * \brief Constructor. 
-	 * \param WXTYPE   
-	 * \param bool  
-	 * \param wxWindowID   
-	 * \param const wxPoint&   
-	*/
-
-   wxHelpEvent(WXTYPE  eventType = 0, bool active = true, wxWindowID  id = 0, const wxPoint&  point ) ;
-	/**
-	 * \brief Returns the left-click position of the mouse, in screen coordinates. This allows
-the application to position the help appropriately. 
-	*/
-
+   wxHelpEvent(WXTYPE eventType = 0, wxWindowID  id = 0, 
+               const wxPoint& point = wxDefaultPosition);
   const wxPoint& GetPosition() const;
-	/**
-	 * \brief Sets the left-click position of the mouse, in screen coordinates. 
-	 * \param const wxPoint&  
-	*/
-
-  void SetPosition(const wxPoint&  pt ) ;
+  void SetPosition(const wxPoint& pt);
 };
 
 
