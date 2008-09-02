c644e6685819e6b17f1b5a2909ea59a3cc670acc
Alex Fenton <alex@pressure.to>
2008-09-02 03:01:27 +0000

    Added missing StaticText#wrap method; cleaned-up header

--- swig/classes/include/wxStaticText.h
+++ swig/classes/include/wxStaticText.h
@@ -1,4 +1,4 @@
-// Copyright 2004-2007, wxRuby development team
+// Copyright 2004-2008, wxRuby development team
 // released under the MIT-like wxRuby2 license
 
 #if !defined(_wxStaticText_h_)
@@ -6,49 +6,26 @@
 class wxStaticText : public wxControl
 {
 public:
-	/**
-	 * \brief Default constructor. 
-	*/
-
    wxStaticText() ;
-	/**
-	 * \brief Constructor, creating and showing a text control. 
-	 * \param wxWindow*   
-	 * \param wxWindowID  
-	 * \param const wxString&   
-	 * \param const wxPoint&   
-	 * \param const wxSize&   
-	 * \param long  
-	 * \param const wxString&   
-	*/
-
-   wxStaticText(wxWindow*  parent , wxWindowID  id , const wxString&  label , const wxPoint&  pos = wxDefaultPosition, const wxSize&  size = wxDefaultSize, long style = 0, const wxString&  name = wxT("staticText")) ;
-	/**
-	 * \brief Creation function, for two-step construction. For details see  . 
-	 * \param wxWindow*   
-	 * \param wxWindowID  
-	 * \param const wxString&   
-	 * \param const wxPoint&   
-	 * \param const wxSize&   
-	 * \param long  
-	 * \param const wxString&   
-	*/
-
-  bool Create(wxWindow*  parent , wxWindowID  id , const wxString&  label , const wxPoint&  pos = wxDefaultPosition, const wxSize&  size = wxDefaultSize, long style = 0, const wxString&  name = wxT("staticText")) ;
-	/**
-	 * \brief Returns the contents of the control. 
-	*/
-
-  virtual wxString GetLabel() const;
-	/**
-	 * \brief Sets the static text label and updates the controls size to exactly fit the
-label unless the control has wxST_NO_AUTORESIZE flag. 
-	 * \param const wxString&   
-	*/
-
-  virtual void SetLabel(const wxString&   label ) ;
-  // the following were missing from the xml
+   wxStaticText(wxWindow* parent, 
+                wxWindowID  id, 
+                const wxString& label, 
+                const wxPoint& pos = wxDefaultPosition, 
+                const wxSize& size = wxDefaultSize, 
+                long style = 0, 
+                const wxString&  name = wxT("staticText") );
   virtual ~wxStaticText();
+
+  bool Create(wxWindow*  parent, 
+              wxWindowID  id, 
+              const wxString& label, 
+              const wxPoint& pos = wxDefaultPosition, 
+              const wxSize& size = wxDefaultSize, 
+              long style = 0, 
+              const wxString& name = wxT("staticText") );
+  wxString GetLabel() const;
+  virtual void SetLabel(const wxString&   label ) ;
+  void Wrap(int width);
 };
 
 
