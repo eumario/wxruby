17e5c0fc74c95c27a30f7d66086c5c8b466b0f1a
Alex Fenton <alex@pressure.to>
2008-11-11 09:28:37 +0000

    Tweaks to Sizer#add_item and ToolBar#add_item, document (Chauk-Mean P)

--- wxruby2/lib/wx/classes/toolbar.rb
+++ wxruby2/lib/wx/classes/toolbar.rb
@@ -5,21 +5,19 @@ class Wx::ToolBar
   ADD_ITEM_PARAMS = [ Wx::Parameter[ :position, -1 ], 
                       Wx::Parameter[ :id, -1 ],
                       Wx::Parameter[ :label, "" ], 
-                      Wx::Parameter[ :bitmap, Wx::NULL_BITMAP ],
                       Wx::Parameter[ :bitmap2, Wx::NULL_BITMAP ],
                       Wx::Parameter[ :kind, Wx::ITEM_NORMAL ], 
                       Wx::Parameter[ :short_help, "" ], 
                       Wx::Parameter[ :long_help, "" ], 
                       Wx::Parameter[ :client_data, nil ] ]
   
-  def add_item(*mixed_args)
+  def add_item(bitmap, *mixed_args)
     args = Wx::args_as_list(ADD_ITEM_PARAMS, *mixed_args)
-    if args[3] == Wx::NULL_BITMAP
-      Kernel.raise ArgumentError, "Main button bitmap may not be NULL"
-    end
+
+    pos = args.shift
+    args.insert(2, bitmap)
 
     # Call add_tool to append if default position
-    pos = args.shift
     if pos == -1
       add_tool(*args)
     else
