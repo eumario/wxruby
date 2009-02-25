a3cb5fc2d3c076f72b76c3a653c2ab49cb6fadea
Chauk-Mean Proum <chauk.mean@gmail.com>
2009-02-25 19:22:46 +0000

    Tweaks to ToolBar#add_item.

--- lib/wx/classes/toolbar.rb
+++ lib/wx/classes/toolbar.rb
@@ -2,20 +2,23 @@
 class Wx::ToolBar
   # Generic method to add items, supporting positional and named
   # arguments
-  ADD_ITEM_PARAMS = [ Wx::Parameter[ :position, -1 ], 
+  ADD_ITEM_PARAMS = [ 
+                      Wx::Parameter[ :bitmap2, Wx::NULL_BITMAP ],
+                      Wx::Parameter[ :position, -1 ], 
                       Wx::Parameter[ :id, -1 ],
                       Wx::Parameter[ :label, "" ], 
-                      Wx::Parameter[ :bitmap2, Wx::NULL_BITMAP ],
                       Wx::Parameter[ :kind, Wx::ITEM_NORMAL ], 
                       Wx::Parameter[ :short_help, "" ], 
                       Wx::Parameter[ :long_help, "" ], 
                       Wx::Parameter[ :client_data, nil ] ]
   
-  def add_item(bitmap, *mixed_args)
+  def add_item(bitmap1, *mixed_args)
     args = Wx::args_as_list(ADD_ITEM_PARAMS, *mixed_args)
 
+    bitmap2 = args.shift
     pos = args.shift
-    args.insert(2, bitmap)
+    args.insert(2, bitmap1)
+    args.insert(3, bitmap2)
 
     # Call add_tool to append if default position
     if pos == -1
