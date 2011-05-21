b95939e1e0ff62549cabd0db29d106ba9be4d5b2
Alex Fenton <alex@pressure.to>
2011-05-21 11:43:26 +0000

    Fix wrong tab-indenting in source and generated code from XRCise

--- wxsugar/lib/wx_sugar/xrc/outputter.rb
+++ wxsugar/lib/wx_sugar/xrc/outputter.rb
@@ -4,56 +4,56 @@ require 'ostruct'
 # Provides templated output of a Ruby subclass from an XRC specification
 # of a ruby Frame, Dialog or Panel class.
 class Outputter
-	# Valid options are :app_name and :module_name
-	attr_accessor :options, :klass
+  # Valid options are :app_name and :module_name
+  attr_accessor :options, :klass
 
-	# Creates a new outputter from the XRCClass +xrc_class+
-	def initialize(xrc_class, options = {})
-		@klass = xrc_class
-		@options = options
-	end
+  # Creates a new outputter from the XRCClass +xrc_class+
+  def initialize(xrc_class, options = {})
+    @klass = xrc_class
+    @options = options
+  end
 
-	# Writes the ruby code to the file or stream +io+
-	def output(io = $stdout)
-		tpl = ERB.new(TEMPLATE)
-		io.puts( tpl.result(binding) )
-	end
+  # Writes the ruby code to the file or stream +io+
+  def output(io = $stdout)
+    tpl = ERB.new(TEMPLATE)
+    io.puts( tpl.result(binding) )
+  end
 
-	# Returns the fully-qualified (including module namespace prefix) for
-	# +name+.
-	def fq_name(name)
-		options[:namespace] ? "#{options[:namespace]}::#{name}" : name
-	end
+  # Returns the fully-qualified (including module namespace prefix) for
+  # +name+.
+  def fq_name(name)
+    options[:namespace] ? "#{options[:namespace]}::#{name}" : name
+  end
 
-	# Returns the options object 
-	def opts
-		OpenStruct.new(options)
-	end
+  # Returns the options object 
+  def opts
+    OpenStruct.new(options)
+  end
 
-	# Utility function to format the attr_readers line so it doesn't wrap
-	# in editors
-	def clean_id_attr_readers(str, start_len = 15, one_per_line = false)
-		new_string = ''
-		
-		if one_per_line
-			str.split(',').each do | x |
-                new_string << "#{x},"
-				new_string << "\n" + " " * (start_len-2)        
-			end
-		else			
-			wanted_length = 72
-			current_line_length = start_len
-			str.split(',').each do | x |
-				if (current_line_length + x.length) > wanted_length
-					new_string << "\n" + " " * (start_len-2)
-					current_line_length = start_len
-				end
-                new_string << "#{x},"
-				current_line_length += x.length      
-			end 
-		end
-		return new_string 
-	end
+  # Utility function to format the attr_readers line so it doesn't wrap
+  # in editors
+  def clean_id_attr_readers(str, start_len = 15, one_per_line = false)
+    new_string = ''
+    
+    if one_per_line
+      str.split(',').each do | x |
+        new_string << "#{x},"
+        new_string << "\n" + " " * (start_len-2)        
+      end
+    else      
+      wanted_length = 72
+      current_line_length = start_len
+      str.split(',').each do | x |
+        if (current_line_length + x.length) > wanted_length
+          new_string << "\n" + " " * (start_len-2)
+          current_line_length = start_len
+        end
+        new_string << "#{x},"
+        current_line_length += x.length      
+      end 
+    end
+    return new_string 
+  end
 end
 
 
@@ -72,42 +72,42 @@ end
 # Generated at: <%= Time.now %>
 
 class <%= fq_name(klass.sub_class) %> < <%= klass.superclass %>
-	<% if not klass.controls.empty? %><% ids = klass.controls.map { | ctrl | ":#{ctrl.name.downcase}" }.join(', ') %>
-	attr_reader <%= clean_id_attr_readers(ids) %>
-	<% end %>
-	def initialize(parent = nil)
-		super()
-		xml = Wx::XmlResource.get
-		xml.flags = 2 # Wx::XRC_NO_SUBCLASSING
-		xml.init_all_handlers
-		xml.load("<%= klass.file_name %>")
-		xml.<%= klass.load_func %>(self, parent, "<%= klass.base_id %>")
+  <% if not klass.controls.empty? %><% ids = klass.controls.map { | ctrl | ":#{ctrl.name.downcase}" }.join(', ') %>
+  attr_reader <%= clean_id_attr_readers(ids) %>
+  <% end %>
+  def initialize(parent = nil)
+    super()
+    xml = Wx::XmlResource.get
+    xml.flags = 2 # Wx::XRC_NO_SUBCLASSING
+    xml.init_all_handlers
+    xml.load("<%= klass.file_name %>")
+    xml.<%= klass.load_func %>(self, parent, "<%= klass.base_id %>")
 
-		finder = lambda do | x | 
-			int_id = Wx::xrcid(x)
-			begin
-				Wx::Window.find_window_by_id(int_id, self) || int_id
-			# Temporary hack to work around regression in 1.9.2; remove
-			# begin/rescue clause in later versions
-			rescue RuntimeError
-				int_id
-			end
-		end
-		<% klass.controls.each do | ctrl | %>
-		@<%= ctrl.name.downcase %> = finder.call("<%= ctrl.name %>")<% if ctrl.sub_class %>
-		@<%= ctrl.name.downcase %>.extend(<%= fq_name(ctrl.sub_class) %>)<% end %><% end %>
-		if self.class.method_defined? "on_init"
-			self.on_init()
-		end
-	end
+    finder = lambda do | x | 
+      int_id = Wx::xrcid(x)
+      begin
+        Wx::Window.find_window_by_id(int_id, self) || int_id
+      # Temporary hack to work around regression in 1.9.2; remove
+      # begin/rescue clause in later versions
+      rescue RuntimeError
+        int_id
+      end
+    end
+    <% klass.controls.each do | ctrl | %>
+    @<%= ctrl.name.downcase %> = finder.call("<%= ctrl.name %>")<% if ctrl.sub_class %>
+    @<%= ctrl.name.downcase %>.extend(<%= fq_name(ctrl.sub_class) %>)<% end %><% end %>
+    if self.class.method_defined? "on_init"
+      self.on_init()
+    end
+  end
 end
 
 <% if opts.app_name %>
 class <%= opts.app_name %> < Wx::App
-	def on_init
-		f =  <%= opts.module_name %>::<%= fq_name(klass.sub_class) %>.new
-		f.show
-	end
+  def on_init
+    f =  <%= opts.module_name %>::<%= fq_name(klass.sub_class) %>.new
+    f.show
+  end
 end
 <%= opts.app_name %>.new.main_loop
 <% end %>
